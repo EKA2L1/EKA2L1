467d19fee136cf18c670b9f0be7049b67030c87c
bentokun <fewdspuckrito@gmail.com>
2018-04-30 09:56:23 +0700

    [sid] Add SIDs using hashes

--- src/emu/common/include/common/cvt.h
+++ src/emu/common/include/common/cvt.h
@@ -1,9 +1,19 @@
 #pragma once
 
 #include <common/types.h>
+#include <iostream>
+#include <sstream>
 
 namespace eka2l1 {
     namespace common {
         std::string ucs2_to_utf8(std::u16string str);
+
+        template <class T>
+        std::string to_string(T t, std::ios_base & (*f)(std::ios_base&))
+        {
+          std::ostringstream oss;
+          oss << f << t;
+          return oss.str();
+        }
     }
 }
--- src/emu/common/include/common/hash.h
+++ src/emu/common/include/common/hash.h
@@ -6,8 +6,7 @@
 namespace eka2l1 {
     namespace common {
         // https://stackoverflow.com/questions/7968674/unexpected-collision-with-stdhash
-        uint32_t hash( std::string const& s ) const
-        {
+        uint32_t hash( std::string const& s ) {
             uint32_t h = 5381;
 
             for (auto c: s)
