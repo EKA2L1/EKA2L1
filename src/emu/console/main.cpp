4408a91bf317e3f412276d9d7a0f6fac008add91
Do Trong Thu <fewdspuckrito@gmail.com>
2018-05-22 12:53:15 +0700

    Add console option for SID path

--- src/emu/console/main.cpp
+++ src/emu/console/main.cpp
@@ -8,6 +8,8 @@
 using namespace eka2l1;
 
 std::string rom_path = "SYM.ROM";
+std::string default_sym_9_4_path = "sym94/db.yml";
+
 eka2l1::system symsys;
 
 bool help_printed = false;
@@ -16,7 +18,9 @@ void print_help() {
     std::cout << "Usage: Drag and drop Symbian file here, ignore missing dependencies" << std::endl;
     std::cout << "Options: " << std::endl;
     std::cout << "-rom: Specified where the ROM is located. If none is specified, the emu will look for a file SYM.ROM" << std::endl; 
-    std::cout << "-h/-help: Print help" << std::endl;
+	std::cout << "-sid: Specified the SID path" << std::endl;
+	std::cout << "\t Follow the SID option is the major version, minor version and path" << std::endl;
+	std::cout << "-h/-help: Print help" << std::endl;
 }
 
 void parse_args(int argc, char** argv) {
@@ -27,11 +31,21 @@ void parse_args(int argc, char** argv) {
             && (!help_printed)) {
             print_help();
             help_printed = true;
-        }
+		}
+		else if (strncmp(argv[i], "-sid", 4) == 0) {
+			int major = std::atoi(argv[++i]);
+			int minor = std::atoi(argv[++i]);
+
+			const char* path = argv[++i];
+			symsys.add_sid(major, minor, path);
+		}
     }
 }
 
 void init() {
+	symsys.set_symbian_version_use(9, 4);
+	symsys.add_sid(9, 4, default_sym_9_4_path);
+
     symsys.init();
     bool res = symsys.load_rom(rom_path);
 }
