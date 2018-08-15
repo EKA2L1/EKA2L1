f5e46845f80a94e58fb8036ba61232571e7a4a94
bentokun <fewdspuckrito@gmail.com>
2018-08-15 19:14:45 +0700

    Fix rebase conflicts

---
+++ miscs/panic/domaincli.py
@@ -0,0 +1,8 @@
+import eka2l1
+
+def panicHook(panicCode):
+    errcode = panicCode & $FFFF
+    line = (panicCode >> 16) & $FFFF
+	
+    emulog('DomainClient exited with exit code: 0x{:x} at line {}', errcode, line)
+	
