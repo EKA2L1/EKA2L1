1b86a55ce276794264591fb7cbe7edcddb1fd73a
bentokun <fewdspuckrito@gmail.com>
2018-09-23 09:17:05 +0700

    [services] Add the domain and domain manager services to init list

--- src/emu/core/include/core/services/domain/database.h
+++ src/emu/core/include/core/services/domain/database.h
@@ -49,17 +49,34 @@ namespace eka2l1 {
         constexpr uint32_t power_hierarchy_trans_timeout = 100000;
         constexpr uint32_t startup_hierarchy_trans_timeout = 10000000;
 
+        // clang-format off
         const static hierarchy power_hierarchy = {
-            hierarchy_power_id, "PowerHierarchy", { { power_root, 0, "PowerRoot", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout }, { apps, power_root, "Apps", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout }, { apps_ui, apps, "AppsUi", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout } }, ETraverseChildrenFirst, ETraverseParentFirst, ETransitionFailureContinue
+            hierarchy_power_id, "PowerHierarchy", { 
+                { power_root, 0, "PowerRoot", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout }, 
+                { apps, power_root, "Apps", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout },
+                { apps_ui, apps, "AppsUi", ESsmStartupSubStateCriticalStatic, power_hierarchy_trans_timeout }
+            }, ETraverseChildrenFirst, ETraverseParentFirst, ETransitionFailureContinue
         };
 
         const static hierarchy startup_hierarchy = {
-            hierarchy_startup_id, "StartupHierarchy", { { kms2_hal_domain_3, 0, "Kms2HALDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_kernel_domain_3, kms2_hal_domain_3, "Kms2KernelDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_os_services_domain_3, kms2_kernel_domain_3, "Kms2OsServicces3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_gen_middleware_domain_3, kms2_os_services_domain_3, "Kms2GenMiddlewareDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_gen_middleware_domain_4, kms2_gen_middleware_domain_3, "Kms2GenMiddlewareDomain4", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_app_services_domain_3, kms2_gen_middleware_domain_4, "Kms2AppServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_app_services_domain_4, kms2_app_services_domain_3, "Kms2AppServicesDomain4", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_ui_services_domain_3, kms2_app_services_domain_4, "Kms2UiServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, { kms2_ui_apps_domain_3, kms2_ui_services_domain_3, "Kms2UiServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout } }, ETraverseParentFirst, ETraverseParentFirst, ETransitionFailureContinue
+            hierarchy_startup_id, "StartupHierarchy", { 
+                { kms2_hal_domain_3, 0, "Kms2HALDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, 
+                { kms2_kernel_domain_3, kms2_hal_domain_3, "Kms2KernelDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, 
+                { kms2_os_services_domain_3, kms2_kernel_domain_3, "Kms2OsServicces3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, 
+                { kms2_gen_middleware_domain_3, kms2_os_services_domain_3, "Kms2GenMiddlewareDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout },
+                { kms2_gen_middleware_domain_4, kms2_gen_middleware_domain_3, "Kms2GenMiddlewareDomain4", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout },
+                { kms2_app_services_domain_3, kms2_gen_middleware_domain_4, "Kms2AppServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, 
+                { kms2_app_services_domain_4, kms2_app_services_domain_3, "Kms2AppServicesDomain4", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout }, 
+                { kms2_ui_services_domain_3, kms2_app_services_domain_4, "Kms2UiServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout },
+                { kms2_ui_apps_domain_3, kms2_ui_services_domain_3, "Kms2UiServicesDomain3", ESsmStartupSubStateCriticalStatic, startup_hierarchy_trans_timeout } 
+            }, ETraverseParentFirst, ETraverseParentFirst, ETransitionFailureContinue
         };
 
         const static std::vector<hierarchy> hierarchies_db = {
             power_hierarchy,
             startup_hierarchy
         };
+
+        // clang-format on
     }
 }
