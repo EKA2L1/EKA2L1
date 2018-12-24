d3123967071d1652e7b7e3fca132d9b0c31a7db7
pent0 <fewdspuckrito@gmail.com>
2018-12-24 19:13:48 +0700

    [services] Specifiy error code with write_arg_pkg
    Appending an external argument in.

--- src/emu/epoc/src/services/context.cpp
+++ src/emu/epoc/src/services/context.cpp
@@ -116,7 +116,7 @@ namespace eka2l1 {
             return false;
         }
 
-        bool ipc_context::write_arg_pkg(int idx, uint8_t *data, uint32_t len) {
+        bool ipc_context::write_arg_pkg(int idx, uint8_t *data, uint32_t len, int *err_code) {
             if (idx >= 4) {
                 return false;
             }
@@ -126,11 +126,16 @@ namespace eka2l1 {
             if ((int)arg_type & (int)ipc_arg_type::flag_des) {
                 // Please don't change the order
                 if ((int)arg_type & (int)ipc_arg_type::flag_16b) {
-                    eka2l1::epoc::TDesC16 *des = static_cast<eka2l1::epoc::TDesC16 *>(
+                    eka2l1::epoc::TDes16 *des = static_cast<eka2l1::epoc::TDes16 *>(
                         msg->own_thr->owning_process()->get_ptr_on_addr_space(msg->args.args[idx]));
                 
                     // We can't handle odd length
                     assert(len % 2 == 0);
+
+                    if (eka2l1::epoc::ExtractDesMaxLength(reinterpret_cast<epoc::TDes8*>(des)) < len) {
+                        err_code ? (*err_code = -2) : 0;
+                        return false;
+                    }
 
                     std::u16string bin;
                     bin.resize(len / 2);
@@ -138,19 +143,26 @@ namespace eka2l1 {
                     memcpy(bin.data(), data, len);
                     des->Assign(msg->own_thr->owning_process(), bin);
                 } else { 
-                    eka2l1::epoc::TDesC8 *des = static_cast<eka2l1::epoc::TDesC8 *>(
+                    eka2l1::epoc::TDes8 *des = static_cast<eka2l1::epoc::TDes8 *>(
                         msg->own_thr->owning_process()->get_ptr_on_addr_space(msg->args.args[idx]));
+
+                    if (eka2l1::epoc::ExtractDesMaxLength(des) < len) {
+                        err_code ? (*err_code = -2) : 0;
+                        return false;
+                    }
 
                     std::string bin;
                     bin.resize(len);
 
                     memcpy(bin.data(), data, len);
+
                     des->Assign(msg->own_thr->owning_process(), bin);
                 }
 
                 return true;
             }
 
+            (err_code) ? (*err_code = -1) : 0;
             return false;
         }
     }
