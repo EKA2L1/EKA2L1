f80437c4db3c9ef7787f58bc17e7ec767dd26082
Do Trong Thu <fewdspuckrito@gmail.com>
2019-01-07 20:44:04 +0700

    epoc/loader: Start working on a RSC loader

--- src/emu/epoc/include/epoc/loader/rsc.h
+++ src/emu/epoc/include/epoc/loader/rsc.h
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team
+ * 
+ * This file is part of EKA2L1 project
+ * (see bentokun.github.com/EKA2L1).
+ * 
+ * Initial contributor: pent0
+ * Contributors:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <cstdint>
@@ -10,13 +33,25 @@
 namespace eka2l1 {
     class file;
     using symfile = std::shared_ptr<file>;
+}
 
+namespace eka2l1::loader {
     class rsc_file_read_stream {
         std::vector<std::uint8_t> buffer;
         common::ro_buf_stream stream;
 
+        enum {
+            potentially_have_compressed_unicode_data = 0x1000,
+            dictionary_compressed = 0x2000,
+            calypso = 0x4000
+        };
+
+        std::uint32_t flags;
+        std::uint16_t size_of_largest_resource_when_uncompressed;
+
 	protected:
         bool do_decompress(symfile f);
+        void read_header_and_resource_index(symfile f);
 
     public:
         explicit rsc_file_read_stream(symfile f, bool *success = nullptr);
---
+++ src/emu/epoc/src/loader/rsc.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team
+ * 
+ * This file is part of EKA2L1 project
+ * (see bentokun.github.com/EKA2L1).
+ * 
+ * Initial contributor: pent0
+ * Contributors:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <epoc/loader/rsc.h>
+#include <epoc/vfs.h>
+
+namespace eka2l1::loader {
+    /*
+       The header format takes like this:
+       - First 12 bytes should contain 3 UID type
+       - The first UID will divide the RSC file into 3 kind:
+          * 0x101f4a6b: This will potentially contains compressed unicode data, which is compressed using run-len
+          * 0x101f5010: This will potentially contains compressed unicode data + will be compressed using dictionary algroithm
+       - If it doesn't fall into 2 of these types, discard all of these read result.
+       - Read the first 2 bytes, if it's equals to 4 than it is fallen into a format called "Calypso" and dictionary compressed.
+    */
+    void rsc_file_read_stream::read_header_and_resource_index(symfile f) {
+        std::uint32_t uid[3];
+        f->read_file(&uid, 4, 3);
+
+        switch (uid[0]) {
+        case 0x101F4A6B: {
+            flags |= potentially_have_compressed_unicode_data;
+
+            f->seek(17, eka2l1::file_seek_mode::beg);
+            f->read_file(&size_of_largest_resource_when_uncompressed, sizeof(size_of_largest_resource_when_uncompressed),
+                1);
+
+            break;
+        }
+
+        case 0x101F5010: {
+            flags |= (potentially_have_compressed_unicode_data | dictionary_compressed);
+            
+            f->seek(17, eka2l1::file_seek_mode::beg);
+            f->read_file(&size_of_largest_resource_when_uncompressed, sizeof(size_of_largest_resource_when_uncompressed),
+                1);
+
+            break;
+        }
+
+        default: {
+            break;
+        }
+        }
+
+        // Check for calypso
+        std::uint16_t calypso_magic = uid[0] >> 16;
+        if (calypso_magic == 4) {
+            f->seek(8, file_seek_mode::beg);
+            f->read_file(&size_of_largest_resource_when_uncompressed, sizeof(size_of_largest_resource_when_uncompressed),
+                1);
+        }
+
+        // Done with the header
+    }
+}
