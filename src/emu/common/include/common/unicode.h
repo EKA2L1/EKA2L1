1d1f323b829ab1154043377c2bfa34b21af49307
pent0 <fewdspuckrito@gmail.com>
2019-01-10 00:02:05 +0700

    common: Add an unicode expander

---
+++ src/emu/common/include/common/unicode.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team
+ * 
+ * This file is part of EKA2L1 project
+ * (see bentokun.github.com/EKA2L1).
+ * 
+ * Initial contributor: pent0
+ * Contributors:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace eka2l1::common {
+    struct unicode_comp_state {
+        enum {
+            static_windows_size = 8,
+            dynamic_windows_size = 8,
+            special_base_size = 7
+        };
+
+        bool unicode_mode;
+        std::uint32_t active_window_base;
+
+        std::uint32_t static_windows[static_windows_size];
+        std::uint32_t dynamic_windows[dynamic_windows_size];
+        std::uint16_t special_bases[special_base_size];
+
+        std::uint32_t dynamic_window_base(int offset_index);
+    }; 
+
+    struct unicode_expander: public unicode_comp_state {
+        int source_pointer;
+        std::uint8_t *source_buf;
+
+        int source_size;
+
+        int dest_pointer;
+        std::uint8_t *dest_buf;
+
+        int dest_size;
+
+        bool read_byte(std::uint8_t *dat);
+        bool write_byte8(std::uint8_t b);
+        bool write_byte(std::uint16_t b);
+        bool write_byte32(std::uint32_t b);
+
+        bool define_window(const int index);
+        bool define_expansion_window();
+
+        bool quote_unicode();
+
+        bool handle_ubyte(const std::uint8_t ubyte);
+        bool handle_sbyte(const std::uint8_t sbyte);
+
+        int expand(std::uint8_t *source, int source_size,
+            std::uint8_t *dest, int dest_size);
+    };
+}
---
+++ src/emu/common/src/unicode.cpp
@@ -0,0 +1,275 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team
+ * 
+ * This file is part of EKA2L1 project
+ * (see bentokun.github.com/EKA2L1).
+ * 
+ * Initial contributor: pent0
+ * Contributors:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common/unicode.h>
+#include <vector>
+
+namespace eka2l1::common {
+    std::uint32_t unicode_comp_state::dynamic_window_base(int offset_index) {
+        if (offset_index >= 0xF9 && offset_index <= 0xFF) {
+            return special_bases[offset_index - 0xF9];
+        }
+
+        if (offset_index >= 0x01 && offset_index <= 0x67) {
+            return offset_index * 0x80;
+        }
+
+        if (offset_index >= 0x68 && offset_index <= 0xA7) {
+            return offset_index * 0x80 + 0xAC00;
+        }
+
+        return 0;
+    }
+
+    bool unicode_expander::read_byte(std::uint8_t *dat) {
+        if (source_size <= 0) {
+            return false;
+        }
+
+        *dat = *(source_buf + (source_pointer++));
+        source_size--;
+
+        return true;
+    }
+
+    bool unicode_expander::write_byte8(std::uint8_t b) {
+        if (dest_size <= 0) {
+            return false;
+        }
+
+        *(dest_buf + (dest_pointer++)) = b;
+        dest_size--;
+
+        return true;
+    }
+
+    bool unicode_expander::write_byte(std::uint16_t b) {
+        if (!write_byte8(b >> 8)) {
+            return false;
+        }
+
+        bool result = write_byte8(static_cast<std::uint8_t>(b));
+        return result;
+    }
+
+    bool unicode_expander::write_byte32(std::uint32_t b) {
+        if (!write_byte8(b >> 24)) {
+            return false;
+        }
+        
+        if (!write_byte8(b >> 16)) {
+            return false;
+        }
+        
+        if (!write_byte8(b >> 8)) {
+            return false;
+        }
+
+        bool result = write_byte8(static_cast<std::uint8_t>(b));
+        return result;
+    }
+
+    bool unicode_expander::define_window(const int index) {
+        std::uint8_t win;
+
+        if (!read_byte(&win)) {
+            return false;
+        }
+
+        unicode_mode = false;
+        active_window_base = dynamic_window_base(win);
+        dynamic_windows[index] = active_window_base;
+
+        return true;
+    }
+
+
+    bool unicode_expander::define_expansion_window() {
+        std::uint8_t hi = 0;
+        std::uint8_t lo = 0;
+
+        if (read_byte(&hi) && read_byte(&lo)) {
+            unicode_mode = false;
+            active_window_base = 0x1000 + (0x80 * ((hi & 0x1F) * 0x100 + lo));
+            dynamic_windows[hi >> 5] = active_window_base;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    enum {
+        UDX = 0xF1,
+        UQU = 0xF0,
+        UD0 = 0xE8,
+        UC0 = 0xE0,
+        SD0 = 0x18,
+        SC0 = 0x10,
+        SCU = 0x0F,
+        SQU = 0x0E,
+        SDX = 0x0B,
+        SQ0 = 0x01
+    };
+
+    bool unicode_expander::quote_unicode() {
+        // Read two bytes
+        std::uint8_t high;
+        std::uint8_t low;
+
+        if (!read_byte(&high)) {
+            return false;
+        }
+
+        if (!read_byte(&low)) {
+            return false;
+        }
+
+        std::uint16_t c = static_cast<std::uint16_t>((high << 8) | low);
+        write_byte(c);
+
+        return true; 
+    }
+    
+    bool unicode_expander::handle_ubyte(const std::uint8_t ubyte) {
+        if (ubyte <= 0xDF || ubyte >= 0xF3) {
+            std::uint8_t low;
+
+            if (read_byte(&low)) {
+                std::uint16_t c = static_cast<std::uint16_t>((ubyte << 8) | low);
+                write_byte(c);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Quote a Unicode character that would otherwise conflict with a tag.
+        if (ubyte == UQU) {
+            return quote_unicode();
+        }
+
+        if (ubyte >= UC0 && ubyte <= UC0 + 7) {
+            active_window_base = dynamic_windows[ubyte - UC0];
+            unicode_mode = false;
+
+            return true;
+        }
+
+        if (ubyte >= UD0 && ubyte <= UD0 + 7) {
+            return define_window(ubyte - UD0);
+        }
+
+        if (ubyte == UDX) {
+            return define_expansion_window();
+        }
+
+        return false;
+    }
+
+    static bool can_i_passthrough(const std::uint16_t sbyte) {
+        return (sbyte == 0x000 || sbyte == 0x0009 || sbyte == 0x000A || sbyte == 0x000D
+            || (sbyte >= 0x0020 && sbyte <= 0x007F));
+    }
+
+    bool unicode_expander::handle_sbyte(const std::uint8_t sbyte) {
+        if (can_i_passthrough(sbyte)) {
+            return write_byte(sbyte);   
+        }
+
+        if (sbyte >= 0x80) {
+            return write_byte32(active_window_base + sbyte - 0x80);
+        }
+
+        if (sbyte == SQU) {
+            return quote_unicode();
+        }
+
+        if (sbyte == SCU) {
+            unicode_mode = true;
+            return true;
+        }
+        
+        // Quote from a window resides from SQ0 to SQ7
+        if (sbyte == UQU) {
+            int window = sbyte - 1;
+            std::uint8_t byte;
+
+            if (!read_byte(&byte)) {
+                return false;
+            }
+
+            std::uint32_t c = byte;
+
+            if (c <= 0x7F) {
+                c += static_windows[window];
+            } else {
+                c += dynamic_windows[window] - 0x80;
+            }
+
+            if (!write_byte32(c)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        if (sbyte >= SC0 && sbyte <= SC0 + 7) {
+            active_window_base = dynamic_windows[sbyte - SC0];
+            return true;
+        }
+
+        if (sbyte >= SD0 && sbyte <= SD0 + 7) {
+            return define_window(sbyte - SD0);
+        }
+
+        if (sbyte == SDX) {
+            return define_expansion_window();
+        }
+
+        return false;
+    }
+
+    int unicode_expander::expand(std::uint8_t *source, int source_size,
+        std::uint8_t *dest, int dest_size) {
+        source_buf = source;
+        dest_buf = dest;
+
+        this->source_size = source_size;
+        this->dest_size = dest_size;
+        
+        for (; source_size >= 0, dest_size >= 0; ) {
+            std::uint8_t b;
+
+            if (read_byte(&b)) {
+                bool result = (unicode_mode ? handle_ubyte(b) : handle_sbyte(b));
+
+                if (!result) {
+                    break;
+                }
+            }
+        }
+
+        return dest_size - this->dest_size;
+    }
+}
