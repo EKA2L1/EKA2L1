a1133d3550250505a52caabdc1fc78728827391e
Do Trong Thu <fewdspuckrito@gmail.com>
2019-02-22 21:46:39 +0700

    manager/script: Include common/algorithm

--- src/emu/manager/src/script_manager.cpp
+++ src/emu/manager/src/script_manager.cpp
@@ -18,6 +18,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <common/algorithm.h>
 #include <common/log.h>
 #include <common/path.h>
 #include <common/platform.h>
