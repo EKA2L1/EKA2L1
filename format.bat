b54dffe6b961d040e4bc7f811ebc2487bcf7d676
Do Trong Thu <fewdspuckrito@gmail.com>
2019-02-24 23:17:33 +0700

    format: Add format file for Windows

---
+++ format.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call :DoFormatOn emu
+call :DoFormatOn tests
+call :DoFormatOn tools
+call :DoFormatOn intests
+
+EXIT /B %ERRORLEVEL%
+
+:DoFormatOn
+    echo Formatting src\%~1
+    cd src\%~1
+    for /f %%f in ('dir *.cpp *.h /b/s') do clang-format -i %%f
+    cd ..\..
+    exit /B 0
