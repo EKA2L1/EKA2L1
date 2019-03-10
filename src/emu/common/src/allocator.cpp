a2d45c74a9fc1d2d9071a986af4988cfe5d9eaac
pent0 <fewdspuckrito@gmail.com>
2019-03-10 20:01:03 +0700

    common/allocator: Decide between two times max and rounded size to be max size

--- src/emu/common/src/allocator.cpp
+++ src/emu/common/src/allocator.cpp
@@ -52,9 +52,11 @@ namespace eka2l1::common {
 
         if (farest_end_offset >= max_size) {
             // It's time to expand
-            if (!expand(max_size * 2)) {
+            if (!expand(common::max(max_size * 2, rounded_size))) {
                 return nullptr;
             }
+
+            max_size = common::max(max_size * 2, rounded_size);
         }
 
         // We try our best, but we can't
