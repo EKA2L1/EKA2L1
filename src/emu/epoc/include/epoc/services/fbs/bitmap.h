fa0e8b154f503535865cce6d0ee57eed1608e6c6
pent0 <fewdspuckrito@gmail.com>
2019-03-12 06:31:14 +0700

    epoc/services/fbs: Add flags field to bitwise_bitmap

--- src/emu/epoc/include/epoc/services/fbs/bitmap.h
+++ src/emu/epoc/include/epoc/services/fbs/bitmap.h
@@ -6,7 +6,13 @@
 
 namespace eka2l1::epoc {
     struct bitwise_bitmap {
+        enum settings_flag {
+            dirty_bitmap = 0x00010000,
+            violate_bitmap = 0x00020000
+        };
+
         uid                uid_;
+        std::uint32_t      flags_;
         eka2l1::ptr<void>  allocator_;
         eka2l1::ptr<void>  pile_;
         int                byte_width_;
