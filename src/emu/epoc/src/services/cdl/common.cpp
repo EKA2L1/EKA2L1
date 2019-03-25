6cc6562c995c0216c91798db143ec1b2e6333317
hgen264 <fewdspuckrito@gmail.com>
2019-03-25 20:02:53 +0700

    epoc/services/cdl: Fix name index data type and string length value
    - Name index is 32-bit integer, but I let it be auto and it choose std::ptrdiff_t, which is 64-bit
    - The size absorbed is the binary size of the string, not the length

--- src/emu/epoc/src/services/cdl/common.cpp
+++ src/emu/epoc/src/services/cdl/common.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <epoc/services/cdl/common.h>
 #include <common/chunkyseri.h>
 
@@ -16,13 +35,23 @@ namespace eka2l1::epoc {
         }
 
         // Now serialize
-        seri.absorb_container(names);
+        seri.absorb_container(names, [](common::chunkyseri &seri, std::u16string &str) {
+            std::uint32_t dat_size = static_cast<std::uint32_t>(str.length()) * 2;
+            seri.absorb(dat_size);
+
+            if (seri.get_seri_mode() == common::SERI_MODE_READ) {
+                str.resize(dat_size / 2);
+            }
+
+            seri.absorb_impl(reinterpret_cast<std::uint8_t*>(&str[0]), dat_size);
+        });
+
         seri.absorb_container(collection_, [&](common::chunkyseri &seri, cdl_ref &ref) {
             seri.absorb(ref.id_);
             seri.absorb(ref.uid_);
 
-            auto name_idx = std::distance(names.begin(),
-                std::lower_bound(names.begin(), names.end(), ref.name_));
+            std::uint32_t name_idx = static_cast<std::uint32_t>(std::distance(names.begin(),
+                std::lower_bound(names.begin(), names.end(), ref.name_)));
 
             seri.absorb(name_idx);
 
