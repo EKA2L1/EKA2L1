d45cb3c6b2091ea1cef9c573ca47bb79b2fe5cdb
pent0 <fewdspuckrito@gmail.com>
2019-03-29 09:38:23 +0700

    scripting/example/codeseg: Rename dll file to euser.dll

--- src/emu/scripting/python/example/kernel/codeseg_load.py
+++ src/emu/scripting/python/example/kernel/codeseg_load.py
@@ -3,7 +3,7 @@ import symemu2.events
 
 def scriptEntry():
     # Load EUSER DLL.
-    seg = symemu.loadCodeseg('euser2.dll')  
+    seg = symemu.loadCodeseg('euser.dll')  
     
     symemu.emulog('Runtime code address: 0x{:X}, size: 0x{:X}'.format(seg.codeRunAddress(), seg.codeSize()))
     symemu.emulog('Runtime data address: 0x{:X}, size: 0x{:X}'.format(seg.dataRunAddress(), seg.dataSize()))
