6f579025f8dc6269dbb415feec3a601aaf1e6df2
pent0 <fewdspuckrito@gmail.com>
2019-03-29 20:00:46 +0700

    common/runlen: Rewrite to template

--- src/emu/common/include/common/runlen.h
+++ src/emu/common/include/common/runlen.h
@@ -22,26 +22,56 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 #include <common/buffer.h>
 
 namespace eka2l1 {
-    /*! \brief Do a 24-bit run-length decompress of the target source buffer.
-     *
-     * Run length is simple and good, easy, used many in Windows and Symbian source
-     * code the old day.
+    /**
+     * \brief Decompress RLE compressed data.
      * 
-     * \param src        Pointer to the compressed data. Must not be null.
-     * \param src_size   Reference to the size of the compressed data. When decompression done,
-     *                   this value is assigned with the number of compressed bytes processed.
-     * \param dest       Pointer to the destination buffer. Can be null, if you want to
-     *                   count the total decompressed bytes.
-     * \param dest_size  Reference contains the maximum size the destination buffer can hold.
-     *                   On returns, this value is assigned with the number of decompressed bytes.
-    */
-    void decompress_rle_24bit(const std::uint8_t *src, std::size_t &src_size,
-        std::uint8_t *dest, std::size_t &dest_size);
+     * Support only 8-bit aligned compression.
+     * 
+     * \param source Read-only source stream of binary data.
+     * \param dest   Write-only destination stream.
+     */
+    template <size_t BIT>
+    void decompress_rle(common::ro_stream *source, common::wo_stream *dest) {
+        static_assert(BIT % 8 == 0, "This RLE decompress function don't support unaligned bit decompress!");
 
-    void decompress_rle_24bit_stream(common::ro_stream *stream, std::size_t &src_size, 
-        std::uint8_t *dest, std::size_t &dest_size);
+        while (source->valid() && dest->valid()) {
+            std::int8_t count8 = 0;
+            source->read(&count8, 1);
+
+            std::int32_t count = count8;
+
+            constexpr int BYTE_COUNT = static_cast<int>(BIT) / 8;
+
+            if (count >= 0) {
+                count = common::min(count, static_cast<const std::int32_t>(dest->left() / BYTE_COUNT));
+                std::uint8_t comp[BYTE_COUNT];
+
+                for (std::size_t i = 0; i < BYTE_COUNT; i++) {
+                    source->read(&comp[i], 1);
+                }
+
+                while (count >= 0) {
+                    for (std::size_t i = 0; i < BYTE_COUNT; i++) {
+                        dest->write(&comp[i], 1);
+                    }
+
+                    count--;
+                }
+            } else {
+                std::uint32_t num_bytes_to_copy = static_cast<std::uint32_t>(count * -BYTE_COUNT);
+                num_bytes_to_copy = common::min(num_bytes_to_copy, static_cast<const std::uint32_t>(dest->left()));
+                
+                std::vector<std::uint8_t> temp_buf_holder;
+                temp_buf_holder.resize(num_bytes_to_copy);
+
+                source->read(&temp_buf_holder[0], num_bytes_to_copy);
+                dest->write(temp_buf_holder.data(), num_bytes_to_copy);   
+            }
+        }
+    }
 }
