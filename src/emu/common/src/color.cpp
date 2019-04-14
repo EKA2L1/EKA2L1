1963b6966abcf0f95e8bac02d8495f2d64d9e7ba
pent0 <fewdspuckrito@gmail.com>
2019-04-15 01:13:42 +0700

    common: Add color list

---
+++ src/emu/common/src/color.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common/color.h>
+#include <cstring>
+
+namespace eka2l1::common::color {
+    const vec_rgb get_color(const char *name) {
+        #define COLOR_DECL(declname, r, g, b) if (strcmp(name, #declname) == 0) return declname;
+        #include <common/color.def>
+
+        return vec_rgb({ 0, 0, 0 });
+    }
+}
