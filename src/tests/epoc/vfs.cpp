d31595237d792e902f761fe220013aaebbdcb0d9
pent0 <fewdspuckrito@gmail.com>
2019-04-30 13:43:03 +0700

    tests/io: Compare ignore case

--- src/tests/epoc/vfs.cpp
+++ src/tests/epoc/vfs.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch.hpp>
+#include <common/algorithm.h>
 #include <common/path.h>
 #include <common/types.h>
 #include <epoc/vfs.h>
@@ -35,7 +36,9 @@ TEST_CASE("get_physical", "vfs") {
     REQUIRE(actual_path_a);
     REQUIRE(actual_path_b);
 
-    REQUIRE(*actual_path_a == std::u16string(u"drive_a") + static_cast<char16_t>(eka2l1::get_separator()) + u"despacito2leak");
+    REQUIRE(eka2l1::common::compare_ignore_case(*actual_path_a, std::u16string(u"drive_a") + static_cast<char16_t>(eka2l1::get_separator()) 
+        + u"despacito2leak") == 0);
 
-    REQUIRE(*actual_path_b == std::u16string(u"drive_b") + static_cast<char16_t>(eka2l1::get_separator()) + u"despacito3leak");
+    REQUIRE(eka2l1::common::compare_ignore_case(*actual_path_b, std::u16string(u"drive_b") + static_cast<char16_t>(eka2l1::get_separator()) 
+        + u"despacito3leak") == 0);
 }
