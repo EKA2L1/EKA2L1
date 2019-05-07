2e82370f4e67e548c5f29ee622cfe8419b7df486
pent0 <fewdspuckrito@gmail.com>
2019-05-07 10:02:48 +0700

    epoc/mem: Add memory map

--- src/emu/epoc/include/epoc/mem/page.h
+++ src/emu/epoc/include/epoc/mem/page.h
@@ -23,6 +23,36 @@
 #include <vector>
 
 namespace eka2l1::mem {
+    enum : uint32_t {
+        null_trap = 0x00000000,
+        local_data = 0x00400000,
+        dll_static_data = 0x38000000,
+        shared_data = 0x40000000,
+        ram_code_addr = 0x70000000,
+        rom = 0x80000000,
+        global_data = 0x90000000,
+        ram_drive = 0xA0000000,
+        mmu = 0xC0000000,
+        io_mapping = 0xC3000000,
+        page_tables = 0xC4000000,
+        umem = 0xC8000000,
+        kernel_mapping = 0xC9200000,
+        rom_eka1 = 0x50000000,
+        ram_code_addr_eka1 = 0xE0000000,
+        ram_code_addr_eka1_end = 0xF0000000,
+        shared_data_eka1 = 0x10000000,
+        shared_data_end_eka1 = 0x30000000,
+        shared_data_section_size_eka2 = 0x30000000,
+        shared_data_section_size_eka1 = 0x20000000,
+        shared_data_section_size_max = shared_data_section_size_eka2,
+        code_seg_section_size = 0x10000000,
+        page_size = 0x1000,
+        page_bits = 12,
+        page_table_number_entries = 1 << (32 - page_bits),
+        shared_data_section_max_number_entries = shared_data_section_size_max / page_size,
+        code_seg_section_number_entries = code_seg_section_size / page_size
+    };
+
     constexpr std::size_t PAGE_PER_TABLE_20B = 0b111111;        ///< Page per table constant for 1MB paging
     constexpr std::size_t PAGE_PER_TABLE_10B = 0b11111111;      ///< Page per table constant for 1KB paging
 
