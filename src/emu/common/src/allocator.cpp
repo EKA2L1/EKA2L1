ac511854fe3972e68ecfef5e149a97c5e3494e5a
pent0 <fewdspuckrito@gmail.com>
2019-05-10 19:52:04 +0700

    common/allocator: Disable alloc optimizations on MSVC release

--- src/emu/common/src/allocator.cpp
+++ src/emu/common/src/allocator.cpp
@@ -1,8 +1,29 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <common/algorithm.h>
 #include <common/allocator.h>
+#include <common/log.h>
 
 #include <algorithm>
 #include <exception>
+#include <iostream>
 #include <stdexcept>
 
 namespace eka2l1::common {
@@ -91,15 +112,22 @@ namespace eka2l1::common {
         : words_((total_bits >> 5) + ((total_bits % 32 != 0) ? 1 : 0), 0xFFFFFFFF) {
     }
 
-    void bitmap_allocator::force_fill(const int offset, const int size, const bool or_mode) {
+    void bitmap_allocator::force_fill(const std::uint32_t offset, const int size, const bool or_mode) {
         std::uint32_t *word = &words_[0] + (offset >> 5);
-        const int set_bit = offset & 31;
-        int end_bit = set_bit + static_cast<int>(size);
+        const std::uint32_t set_bit = offset & 31;
+        int end_bit = static_cast<std::uint32_t>(set_bit + size);
+
+        std::uint32_t wval = *word;
 
         if (end_bit < 32) {
             // The bit we need to allocate is in single word
-            const std::uint32_t mask = (~(0xFFFFFFFFU >> size) >> set_bit);
-            *word = or_mode ? (*word | mask) : (*word & ~mask);
+            const std::uint32_t mask = ((~(0xFFFFFFFFU >> size)) >> set_bit);
+
+            if (or_mode) {
+                *word = wval | mask;
+            } else {
+                *word = wval & (~mask);
+            }
 
             return;
         }
@@ -108,8 +136,15 @@ namespace eka2l1::common {
         std::uint32_t mask = 0xFFFFFFFFU >> set_bit;
 
         while (end_bit > 0) {
-            const std::uint32_t w = *word;
-            *word++ = or_mode ? (w | mask) : (w & ~mask);
+            wval = *word;
+
+            if (or_mode) {
+                *word = wval | mask;
+            } else {
+                *word = wval & (~mask);
+            }
+            
+            word += 1; 
 
             // We only need to be careful with the first word, since it only fills
             // some first bits. We should fully fill with other word, so set the mask full
@@ -117,21 +152,26 @@ namespace eka2l1::common {
             end_bit -= 32;
 
             if (end_bit < 32) {
-                mask = ~(mask >> end_bit);
+                mask = ~(mask >> static_cast<std::uint32_t>(end_bit));
             }
         }
     }
 
-    void bitmap_allocator::free(const int offset, const int size) {
+    void bitmap_allocator::free(const std::uint32_t offset, const int size) {
         force_fill(offset, size, true);
     }
 
-    int bitmap_allocator::allocate_from(const int start_offset, const int size, const bool best_fit) {
+// Release code generation is corrupted somewhere on MSVC. Force fill is good so i guess it's the other.
+// Either way, until when i can repro this in a short code, files and bug got fixed, this stays here.
+#ifdef _MSC_VER
+#pragma optimize("", off)
+#endif
+    int bitmap_allocator::allocate_from(const std::uint32_t start_offset, const int size, const bool best_fit) {
         std::uint32_t *word = &words_[0] + (start_offset << 5);
 
         // We have arrived at le word that still have free position (bit 1)
         std::uint32_t *word_end = &words_[words_.size() - 1];
-        int soc = start_offset;
+        std::uint32_t soc = start_offset;
 
         int bflmin = 0xFFFFFF;
         int bofmin = -1;
@@ -147,8 +187,7 @@ namespace eka2l1::common {
                 int boff = 0;
                 std::uint32_t *bword = nullptr;
 
-                // Start from the most significant bit 1
-                int cursor = common::max(31 - soc & 31, common::find_most_significant_bit_one(wv) - 1);
+                int cursor = 31;
 
                 while (cursor > 0) {
                     if (((wv >> cursor) & 1) == 1) {
@@ -160,9 +199,10 @@ namespace eka2l1::common {
                             bflen++;
                             cursor--;
 
-                            if (cursor < 0 && ++word <= word_end) {
+                            if (cursor < 0 && word + 1 <= word_end) {
                                 cursor = 31;
                                 soc = 32;
+                                word++;
                                 wv = *word;
                             }
                         }
@@ -171,7 +211,7 @@ namespace eka2l1::common {
                             if (!best_fit) {
                                 // Force allocate and then return
                                 const int offset = static_cast<int>(31 - boff + ((bword - &words_[0]) << 5));
-                                force_fill(offset, size, false);
+                                force_fill(static_cast<const std::uint32_t>(offset), size, false);
 
                                 return offset;
                             } else {
@@ -195,13 +235,17 @@ namespace eka2l1::common {
         if (best_fit && bofmin) {
             // Force allocate and then return
             const int offset = static_cast<int>(31 - bofmin + ((wordmin - &words_[0]) << 5));
-            force_fill(offset, size, false);
+            force_fill(static_cast<std::uint32_t>(offset), size, false);
 
             return offset;
         }
     
         return -1;
     }
+    
+#ifdef _MSC_VER
+#pragma optimize("", on)
+#endif
 
     bool bitmap_allocator::set_word(const std::uint32_t off, const std::uint32_t val) {
         if (off >= words_.size()) {
@@ -214,7 +258,7 @@ namespace eka2l1::common {
 
     const std::uint32_t bitmap_allocator::get_word(const std::uint32_t off) const {
         if (off >= words_.size()) {
-            return false;
+            return 0;
         }
 
         return words_[off];
