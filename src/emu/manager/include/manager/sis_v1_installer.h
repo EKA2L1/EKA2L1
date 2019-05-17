f2ef3555c89a2ee78128e50a846611c936186332
pent0 <fewdspuckrito@gmail.com>
2019-05-17 18:49:18 +0700

    manager/sis_v1_installer: Set name from component name

--- src/emu/manager/include/manager/sis_v1_installer.h
+++ src/emu/manager/include/manager/sis_v1_installer.h
@@ -11,5 +11,17 @@ namespace eka2l1 {
 }
 
 namespace eka2l1::loader {
-    bool install_sis_old(std::u16string path, io_system *io, drive_number drive, epocver sis_ver, manager::app_info &info);
+    /**
+     * \brief Parse and install old SIS format file.
+     * 
+     * \param path          Path to the package.
+     * \param io            Pointer to IO system.
+     * \param drive         Target drive to install the package.
+     * \param info          The package info to fill in.
+     * \param package_files A vector that will be filled with installed file paths.
+     * 
+     * \returns True on success.
+     */
+    bool install_sis_old(const std::u16string &path, io_system *io, drive_number drive, 
+        manager::package_info &info, std::vector<std::u16string> &package_files);
 }
