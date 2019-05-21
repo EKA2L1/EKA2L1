c0580e207c5910ab2766e7a8811a5bb055c8316c
pent0 <fewdspuckrito@gmail.com>
2019-05-21 18:21:09 +0700

    manager/package: Add file bucket and integrate new SIS interpreter

--- src/emu/manager/include/manager/package_manager.h
+++ src/emu/manager/include/manager/package_manager.h
@@ -20,8 +20,10 @@
 #pragma once
 
 #include <common/types.h>
+#include <manager/sis_common.h>
 
-#include <map>
+#include <atomic>
+#include <mutex>
 #include <optional>
 #include <fstream>
 #include <string>
@@ -37,6 +39,7 @@ namespace eka2l1 {
     /*! \brief Managing apps. */
     namespace manager {
         using uid = uint32_t;
+        struct config_state;
 
         struct package_info {
             std::u16string name;
@@ -48,19 +51,24 @@ namespace eka2l1 {
 
         // A package manager, serves for managing packages
         class package_manager {
-            std::map<uid, package_info> packages;
+            std::vector<package_info> packages;
 
             bool load_sdb_yaml(const std::string &path);
             bool write_sdb_yaml(const std::string &path);
 
             io_system *io;
+            config_state *conf;
 
             bool install_controller(loader::sis_controller *ctrl, drive_number drv);
-            bool add_to_file_bucket(const uid package_uid, const std::string &path);
+            void add_package(package_info &pkg);
 
         public:
+            mutable std::mutex lockdown;
+            loader::show_text_func show_text;
+            loader::choose_lang_func choose_lang;
+
             package_manager() = default;
-            package_manager(io_system *io);
+            package_manager(io_system *io, config_state *conf);
 
             bool installed(const uid pkg_uid);
 
@@ -68,7 +76,15 @@ namespace eka2l1 {
                 return packages.size();
             }
 
-            bool install_package(const std::u16string &path, drive_number drive);
+            // No thread safe
+            const package_info *package(const std::size_t idx) const;
+            void get_file_bucket(const manager::uid pkg_uid, std::vector<std::string> &paths);
+            bool add_to_file_bucket(const uid package_uid, const std::string &path);
+            void delete_files_and_bucket(const uid package_uid);
+
+            bool install_package(const std::u16string &path, const drive_number drive,
+                std::atomic<int> &progress);
+
             bool uninstall_package(const uid app_uid);
         };
     }
