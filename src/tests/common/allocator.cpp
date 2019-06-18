b8a87d85e04ba696b86a325af73831419e2d5730
pent0 <fewdspuckrito@gmail.com>
2019-06-18 12:18:20 +0700

    tests/common: Reflect bitmap allocator test with new changes

--- src/tests/common/allocator.cpp
+++ src/tests/common/allocator.cpp
@@ -30,7 +30,10 @@ TEST_CASE("bitmap_alloc_no_best_fit_only_one_fit", "bitmap_allocator") {
 
     // Bitmap:              1000 0101 0001 0001 0101 0001 00[11 1]001
     alloc.set_word(0, 0b10000101000100010101000100111001);
-    REQUIRE(alloc.allocate_from(0, 3) == 26);
+
+    int to_alloc = 3;
+    REQUIRE(alloc.allocate_from(0, to_alloc) == 26);
+    REQUIRE(to_alloc == 3);
 
     // After allocate:      1000 0101 0001 0001 0101 0001 00[00 0]001
     REQUIRE(alloc.get_word(0) == 0b10000101000100010101000100000001);
@@ -41,7 +44,10 @@ TEST_CASE("bitmap_alloc_no_best_fit_multiple_fit", "bitmap_allocator") {
 
     // Bitmap 1:            1000 0[111] 1001 0001 0101 0001 0011 1001
     alloc.set_word(0, 0b10000111100100010101000100111001);
-    REQUIRE(alloc.allocate_from(0, 3) == 5);
+
+    int to_alloc = 3;
+    REQUIRE(alloc.allocate_from(0, to_alloc) == 5);
+    REQUIRE(to_alloc == 3);
 
     // After allocate:      1000 0[000] 1001 0001 0101 0001 0011 1001
     REQUIRE(alloc.get_word(0) == 0b10000000100100010101000100111001);
@@ -53,7 +59,9 @@ TEST_CASE("bitmap_alloc_no_best_fit_alloc_across", "bitmap_allocator") {
     alloc.set_word(0, 0b111);
     alloc.set_word(1, 0b11100000000000000000000000000000);
 
-    REQUIRE(alloc.allocate_from(0, 5) == 29);
+    int to_alloc = 5;
+    REQUIRE(alloc.allocate_from(0, to_alloc) == 29);
+    REQUIRE(to_alloc == 5);
 
     REQUIRE(alloc.get_word(0) == 0);
     REQUIRE(alloc.get_word(1) == 0b00100000000000000000000000000000);
@@ -64,7 +72,10 @@ TEST_CASE("bitmap_alloc_best_fit_multiple_fit", "bitmap_allocator") {
 
     // Bitmap 1:            1000 0111 1001 0001 0101 0001 00[11 1]001
     alloc.set_word(0, 0b10000111100100010101000100111001);
-    REQUIRE(alloc.allocate_from(0, 3, true) == 26);
+
+    int to_alloc = 3;
+    REQUIRE(alloc.allocate_from(0, to_alloc, true) == 26);
+    REQUIRE(to_alloc == 3);
 
     // After allocate:      1000 0111 1001 0001 0101 0001 00[00 0]001
     REQUIRE(alloc.get_word(0) == 0b10000111100100010101000100000001);
