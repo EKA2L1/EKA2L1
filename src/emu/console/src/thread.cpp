463012230bee2f1c9271ee9126936daa1926ed95
pent0 <fewdspuckrito@gmail.com>
2019-09-18 06:22:55 +0700

    console/thread: Only enable SEH catching on release

--- src/emu/console/src/thread.cpp
+++ src/emu/console/src/thread.cpp
@@ -373,7 +373,7 @@ namespace eka2l1::desktop {
 
     void os_thread(emulator &state) {
         // Register SEH handler for this thread
-#if EKA2L1_PLATFORM(WIN32) && defined(_MSC_VER) && ENABLE_SEH_HANDLER
+#if EKA2L1_PLATFORM(WIN32) && defined(_MSC_VER) && ENABLE_SEH_HANDLER && defined(NDEBUG)
         _set_se_translator(seh_handler_translator_func);
 #endif
 
