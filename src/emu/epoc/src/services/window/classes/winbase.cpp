4e991d10e52b83611c01f40bf05ebe7866946c4c
pent0 <fewdspuckrito@gmail.com>
2020-02-12 10:45:36 +0700

    epoc/services/window: Correct walk tree back to front again
    We didnt iterate to the oldest sibling, just oldest child. Now we do

--- src/emu/epoc/src/services/window/classes/winbase.cpp
+++ src/emu/epoc/src/services/window/classes/winbase.cpp
@@ -198,14 +198,11 @@ namespace eka2l1::epoc {
 
         // Gone through all siblings, walk on their childs
         // TODO: Is this correct? Older guy, children of older guy, then newer guy, children of newer guy.
-        while (start != nullptr) {
-            walk_tree_back_to_front(start->child, walker);
-    
-            if (walker->do_it(start)) {
-                return;
-            }
-            
-            start = start->sibling;
+        walk_tree_back_to_front(start->sibling, walker);
+        walk_tree_back_to_front(start->child, walker);
+
+        if (walker->do_it(start)) {
+            return;
         }
     }
 
