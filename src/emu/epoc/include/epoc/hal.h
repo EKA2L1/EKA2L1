934466b49cacd8af808692aeae1daec94fc4cb19
pent0 <fewdspuckrito@gmail.com>
2020-02-27 22:31:17 +0700

    epoc/hal: Add support for multiple device and display HAL overhaul

--- src/emu/epoc/include/epoc/hal.h
+++ src/emu/epoc/include/epoc/hal.h
@@ -1,22 +1,21 @@
 /*
-* Copyright (c) 2018 EKA2L1 Team / 2009 Nokia Corporation
-*
-* This file is part of EKA2L1 project / 2009 Nokia Corporation
-* (see bentokun.github.com/EKA2L1).
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (c) 2018 EKA2L1 Team / 2009 Nokia Corporation
+ *
+ * This file is part of EKA2L1 project / 2009 Nokia Corporation
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -160,39 +159,42 @@ namespace eka2l1::epoc {
         uint32_t iSpeedFactor;
     };
 
-    class TVideoInfoV01 {
+    class video_info_v1 {
     public:
-        vec2 iSizeInPixels; /**< The visible width/height of the display in pixels. */
-        vec2 iSizeInTwips; /**< The visible width/height of the display in twips. */
-        bool iIsMono; /**< True if display is monochrome; false otherwise. */
-        bool iIsPalettized; /**< True if display is palettized (in current display mode); false otherwise. */
-        int iBitsPerPixel; /**< The number of bits in one pixel. */
-        int iVideoAddress; /**< The virtual address of screen memory. */
-        int iOffsetToFirstPixel; /**< Number of bytes from iVideoAddress for the first displayed pixel. */
-        int iOffsetBetweenLines; /**< Number of bytes between start of one line and start of next. */
-        bool iIsPixelOrderRGB; /**< The orientation of sub pixels on the screen; True if RBG, False if BGR. */
-        bool iIsPixelOrderLandscape; /**< True if display pixels are landscape. */
-        int iDisplayMode; /**< The current display mode. */
+        vec2 size_in_pixels_;
+        vec2 size_in_twips_;
+        bool is_mono_;
+        bool is_palettelized_;
+        std::int32_t bits_per_pixel_;
+        std::uint32_t video_address_;
+        std::int32_t offset_to_first_pixel_;
+        std::int32_t offset_between_lines_;
+        bool is_pixel_order_rgb_;
+        bool is_pixel_order_landspace_;
+        std::int32_t display_mode_;
     };
 
     /*! \brief A HAL function. Each function has minimum of 0 arg and maximum of 2 args. */
-    using hal_func = std::function<int(int *, int *)>;
+    using hal_func = std::function<int(int *, int *, std::uint16_t)>;
     using hal_cagetory_funcs = std::unordered_map<uint32_t, hal_func>;
 
-    /*! \brief Base class for all High Level Abstraction Layer Cagetory. */
+    /**
+     * \brief Base class for all High Level Abstraction Layer Cagetory.
+     */
     struct hal {
         hal_cagetory_funcs funcs;
         eka2l1::system *sys;
 
     public:
-        hal(eka2l1::system *sys);
+        explicit hal(eka2l1::system *sys);
 
-        /*! \brief Execute an HAL function. 
+        /*
+         * \brief Execute an HAL function. 
          *
          * \param func Function opcode.
          * \returns HAL operation result.
          */
-        int do_hal(uint32_t func, int *a1, int *a2);
+        int do_hal(uint32_t func, int *a1, int *a2, const std::uint16_t device_number);
     };
 
     /*! \brief Initalize HAL. 
--- src/emu/epoc/src/hal.cpp
+++ src/emu/epoc/src/hal.cpp
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2018 EKA2L1 Team.
  * 
- * This file is part of EKA2L1 project 
- * (see bentokun.github.com/EKA2L1).
- * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -22,6 +19,7 @@
 #include <epoc/hal.h>
 #include <epoc/kernel.h>
 #include <epoc/timing.h>
+#include <epoc/services/window/window.h>
 
 #include <epoc/loader/rom.h>
 
@@ -36,7 +34,7 @@
 #include <manager/config.h>
 
 #define REGISTER_HAL_FUNC(op, hal_name, func) \
-    funcs.emplace(op, std::bind(&hal_name::func, this, std::placeholders::_1, std::placeholders::_2))
+    funcs.emplace(op, std::bind(&hal_name::func, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3))
 
 #define REGISTER_HAL_D(sys, cage, hal_name) \
     std::unique_ptr<epoc::hal> hal_com = std::make_unique<hal_name>(sys);   \
@@ -50,32 +48,35 @@ namespace eka2l1::epoc {
     hal::hal(eka2l1::system *sys)
         : sys(sys) {}
 
-    int hal::do_hal(uint32_t func, int *a1, int *a2) {
+    int hal::do_hal(std::uint32_t func, int *a1, int *a2, const std::uint16_t device_num) {
         auto func_pair = funcs.find(func);
 
         if (func_pair == funcs.end()) {
             return -1;
         }
 
-        return func_pair->second(a1, a2);
+        return func_pair->second(a1, a2, device_num);
     }
 
-    /*! \brief Kernel HAL cagetory. 
+    /**
+     * \brief Kernel HAL cagetory. 
+     * 
      * Contains HAL of drivers, memory, etc...
      */
     struct kern_hal : public eka2l1::epoc::hal {
-        /*! \brief Get the size of a page. 
+        /**
+         * \brief Get the size of a page. 
          *
          * \param a1 The pointer to the integer destination, supposed to
          *           contains the page size.
          * \param a2 Unused.
          */
-        int page_size(int *a1, int *a2) {
+        int page_size(int *a1, int *a2, const std::uint16_t device_num) {
             *a1 = static_cast<int>(sys->get_memory_system()->get_page_size());
             return epoc::error_none;
         }
 
-        int memory_info(int *a1, int *a2) {
+        int memory_info(int *a1, int *a2, const std::uint16_t device_num) {
             des8 *buf = reinterpret_cast<des8 *>(a1);
             TMemoryInfoV1 memInfo;
 
@@ -96,7 +97,7 @@ namespace eka2l1::epoc {
             return epoc::error_none;
         }
 
-        kern_hal(eka2l1::system *sys)
+        explicit kern_hal(eka2l1::system *sys)
             : hal(sys) {
             REGISTER_HAL_FUNC(EKernelHalPageSizeInBytes, kern_hal, page_size);
             REGISTER_HAL_FUNC(EKernelHalMemoryInfo, kern_hal, memory_info);
@@ -104,7 +105,7 @@ namespace eka2l1::epoc {
     };
 
     struct variant_hal : public eka2l1::epoc::hal {
-        int get_variant_info(int *a1, int *a2) {
+        int get_variant_info(int *a1, int *a2, const std::uint16_t device_num) {
             epoc::des8 *package = reinterpret_cast<epoc::des8 *>(a1);
             epoc::TVariantInfoV1 *info_ptr = reinterpret_cast<epoc::TVariantInfoV1 *>(
                 package->get_pointer(sys->get_kernel_system()->crr_process()));
@@ -127,33 +128,61 @@ namespace eka2l1::epoc {
     };
 
     struct display_hal : public hal {
-        int current_mode_info(int *a1, int *a2) {
+        window_server *winserv_;
+        
+        int current_mode_info(int *a1, int *a2, const std::uint16_t device_num) {
+            assert(winserv_);
+
             epoc::des8 *package = reinterpret_cast<epoc::des8 *>(a1);
-            epoc::TVideoInfoV01 *info_ptr = reinterpret_cast<epoc::TVideoInfoV01 *>(
+            epoc::video_info_v1 *info_ptr = reinterpret_cast<epoc::video_info_v1 *>(
                 package->get_pointer(sys->get_kernel_system()->crr_process()));
 
-            info_ptr->iSizeInPixels = eka2l1::vec2(sys->get_config()->display_size_x_pixs, sys->get_config()->display_size_y_pixs);
-            info_ptr->iSizeInTwips = info_ptr->iSizeInPixels * 15;
-            info_ptr->iIsMono = false; // This can be changed
-            info_ptr->iBitsPerPixel = 3;
-            info_ptr->iIsPixelOrderRGB = true;
-            info_ptr->iIsPixelOrderLandscape = false; // This can be changed
+            epoc::screen *scr = winserv_->get_screen(device_num);
+
+            if (!scr) {
+                return epoc::error_not_found;
+            }
+
+            info_ptr->size_in_pixels_ = scr->size();
+            info_ptr->size_in_twips_ = info_ptr->size_in_pixels_ * 15;
+            info_ptr->is_mono_ = is_display_mode_mono(scr->disp_mode);
+            info_ptr->bits_per_pixel_ = get_bpp_from_display_mode(scr->disp_mode);
+            info_ptr->is_pixel_order_rgb_ = (scr->disp_mode >= epoc::display_mode::color4k);
+
+            // TODO: Verify
+            info_ptr->is_pixel_order_landspace_ = (scr->size().x > scr->size().y);
+            info_ptr->is_palettelized_ = !info_ptr->is_mono_ && (scr->disp_mode < epoc::display_mode::color4k);
+            
+            // Intentional
+            info_ptr->offset_to_first_pixel_ = 0x10000;
+            info_ptr->display_mode_ = static_cast<std::int32_t>(scr->disp_mode);
+
+            LOG_TRACE("{}", info_ptr->video_address_);
 
             return 0;
         }
 
-        int color_count(int *a1, int *a2) {
-            *a1 = 1 << 24;
+        int color_count(int *a1, int *a2, const std::uint16_t device_num) {
+            assert(winserv_);
+            
+            epoc::screen *scr = winserv_->get_screen(device_num);
 
-            LOG_WARN("Color count stubbed with 1 << 24 (24 bit).");
+            if (!scr) {
+                return epoc::error_not_found;
+            }
 
+            *a1 = get_num_colors_from_display_mode(scr->disp_mode);
             return 0;
         }
 
-        display_hal(system *sys)
-            : hal(sys) {
+        explicit display_hal(system *sys)
+            : hal(sys)
+            , winserv_(nullptr) {
             REGISTER_HAL_FUNC(EDisplayHalCurrentModeInfo, display_hal, current_mode_info);
             REGISTER_HAL_FUNC(EDisplayHalColors, display_hal, color_count);
+            
+            winserv_ = reinterpret_cast<window_server*>(sys->get_kernel_system()
+                ->get_by_name<service::server>(WINDOW_SERVER_NAME));
         }
     };
 
@@ -164,7 +193,7 @@ namespace eka2l1::epoc {
     }
 
     int do_hal(eka2l1::system *sys, uint32_t cage, uint32_t func, int *a1, int *a2) {
-        hal *hal_com = sys->get_hal(cage);
+        hal *hal_com = sys->get_hal(static_cast<std::uint16_t>(cage));
 
         if (!hal_com) {
             LOG_TRACE("HAL cagetory not found or unimplemented: 0x{:x} (for function: 0x{:x})",
@@ -173,7 +202,7 @@ namespace eka2l1::epoc {
             return epoc::error_not_found;
         }
 
-        int ret = hal_com->do_hal(func, a1, a2);
+        int ret = hal_com->do_hal(func, a1, a2, static_cast<std::uint16_t>(cage >> 16));
 
         if (ret == -1) {
             LOG_WARN("Unimplemented HAL function, cagetory: 0x{:x}, function: 0x{:x}",
