7d3ca0add61a52a0a345f2ea057b12623bd10eaf
pent0 <fewdspuckrito@gmail.com>
2020-03-11 17:01:42 +0700

    emu/patch/scdv: Add supervisor HLE dispatch

---
+++ src/patch/scdv/inc/scdv/sv.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCDV_SV_H_
+#define SCDV_SV_H_
+
+#include <e32std.h>
+
+#define CALL_HLE_DISPATCH(FUNCID)			\
+	asm volatile("mov r0, #" #FUNCID);		\
+	asm volatile("swi #0xFE");
+
+volatile void UpdateScreen(const TUint32 aNumber, const TRect *aRectangles) {
+	CALL_HLE_DISPATCH(1);
+}
+
+#endif
