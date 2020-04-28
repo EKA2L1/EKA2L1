95ed59a2ccfe7c7f136e3037e0dc3dd158a307a4
hawkwood304 <62043085+hawkwood304@users.noreply.github.com>
2020-04-28 17:51:07 +0700

    patch/scdv: Add Direct Screen Bitmap implementation

--- src/patch/scdv/src/cdsb.cpp
+++ src/patch/scdv/src/cdsb.cpp
@@ -22,13 +22,86 @@
 #include <cdsb.h>
 #include <scdv/log.h>
 #include <scdv/panic.h>
+#include <scdv/sv.h>
+
+#include <hal.h>
+
+struct CDirectScreenBitmapImpl: public CDirectScreenBitmap {
+    TUint32 *iData;
+    TSize iSize;
+    TUint32 iFlags;
+    TInt iScreenNum;
+
+public:
+    explicit CDirectScreenBitmapImpl();
+
+    virtual TInt Create(const TRect& aScreenRect, TSettingsFlags aSettingsFlags);
+    virtual TInt BeginUpdate(TAcceleratedBitmapInfo& aBitmapInfo);
+    virtual void EndUpdate(TRequestStatus& aComplete);
+    virtual void EndUpdate(const TRect& aScreenRect, TRequestStatus& aComplete);
+    virtual void Close();
+};
+
+CDirectScreenBitmapImpl::CDirectScreenBitmapImpl()
+    : iData(NULL)
+    , iSize(0, 0)
+    , iFlags(0)
+    , iScreenNum(0) {
+}
+
+TInt CDirectScreenBitmapImpl::Create(const TRect& aScreenRect, TSettingsFlags aSettingsFlags) {
+    const TInt screenNum = (aSettingsFlags >> 28) & 7;
+    HAL::Get(screenNum, HAL::EDisplayMemoryAddress, (TInt &)(iData));
+
+    if (iData == NULL) {
+        return KErrArgument;
+    }
+
+    // Try to get the width and the height of the screen
+    HAL::Get(screenNum, HAL::EDisplayXPixels, iSize.iWidth);
+    HAL::Get(screenNum, HAL::EDisplayYPixels, iSize.iHeight);
+
+    // Getting the real flags, ignoring the screen number
+    iFlags = (aSettingsFlags & ~(7 << 28));
+    iScreenNum = screenNum;
+
+    Scdv::Log("New direct screen bitmap created for screen %d, double buffer: %d", screenNum, iFlags & CDirectScreenBitmap::EDoubleBuffer);
+
+    return KErrNone;
+}
+
+TInt CDirectScreenBitmapImpl::BeginUpdate(TAcceleratedBitmapInfo& aBitmapInfo) {
+    aBitmapInfo.iDisplayMode = EColor16MA;
+    aBitmapInfo.iAddress = reinterpret_cast<TUint8*>(iData);
+    aBitmapInfo.iSize = iSize;
+    aBitmapInfo.iLinePitch = iSize.iWidth * 4;
+    aBitmapInfo.iPixelShift = 5;    // Should be 2^5 = 32
+
+    return KErrNone;
+}
+
+void CDirectScreenBitmapImpl::EndUpdate(TRequestStatus& aComplete) {
+    TRect rect;
+    rect.iTl = TPoint(0, 0);
+    rect.iBr = rect.iTl + iSize;
+
+    EndUpdate(rect, aComplete);
+}
+
+void CDirectScreenBitmapImpl::EndUpdate(const TRect& aScreenRect, TRequestStatus& aComplete) {
+    UpdateScreen(1, iScreenNum, 1, &aScreenRect);
+
+    TRequestStatus *status = &aComplete;
+    User::RequestComplete(status, KErrNone);
+}
+
+void CDirectScreenBitmapImpl::Close() {
+}
 
 CDirectScreenBitmap *CDirectScreenBitmap::NewL() {
     return CDirectScreenBitmap::NewL(0);
 }
 
 CDirectScreenBitmap *CDirectScreenBitmap::NewL(const TInt aScreenNum) {
-    Scdv::Log("ERR:: Unsupported creating direct screen bitmap, TODO!");
-    Scdv::Panic(Scdv::EPanicUnsupported);
-    return NULL;
+    return new (ELeave) CDirectScreenBitmapImpl();
 }
