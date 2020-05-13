09f96ac0e1acf7528d5d3669d9ffb1d2f90b13fc
pent0 <fewdspuckrito@gmail.com>
2020-05-13 15:21:42 +0700

    epoc/kernel/libmngr: Relocate data either with bss or data available
    Fix snake 3d crash

--- src/emu/epoc/src/kernel/libmanager.cpp
+++ src/emu/epoc/src/kernel/libmanager.cpp
@@ -217,7 +217,8 @@ namespace eka2l1::hle {
 
         relocate(img->code_reloc_section.entries, code_base, code_delta, data_delta);
 
-        if (img->header.bss_size) {
+        // Either .data or .bss exists, we need to relocate. Sometimes .data needs relocate too!
+        if ((img->header.bss_size) || (img->header.data_size)) {
             img->rt_data_addr = rtdata_addr;
             relocate(img->data_reloc_section.entries, data_base, code_delta, data_delta);
         }
