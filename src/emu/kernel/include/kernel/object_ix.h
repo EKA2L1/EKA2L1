cdf7facb081488091509cc88d09a95cf416ace7e
aikawa <003231321202@qq.com>
2020-07-10 15:38:15 +0700

    kernel/object_ix: Add has and count utility

--- src/emu/kernel/include/kernel/object_ix.h
+++ src/emu/kernel/include/kernel/object_ix.h
@@ -62,23 +62,33 @@ namespace eka2l1 {
             kernel_system *kern;
 
         public:
-            object_ix() {}
-            object_ix(kernel_system *kern, handle_array_owner owner);
+            explicit object_ix() {}
+            explicit object_ix(kernel_system *kern, handle_array_owner owner);
 
             void do_state(common::chunkyseri &seri);
 
-            /*! \brief Add new object to the ix. 
-             * \returns Handle to the object
+            /**
+             * @brief   Add new object to the container.
+             * 
+             * @param   obj     Pointer to the kernel object.
+             * @returns Handle to the object
             */
             std::uint32_t add_object(kernel_obj_ptr obj);
 
-            /*! \brief Duplicate an existing object handle. 
-                \returns New handle. 0 if fail to create.
+            /**
+             * @brief   Duplicate an existing object handle. 
+             * 
+             * @param   handle The existing handle to duplicate.
+             * 
+             * @returns New handle. 0 if fail to create.
             */
             std::uint32_t duplicate(uint32_t handle);
 
-            /*! \brief Get the kernel object reference by the handle. 
-                \returns The kernel object referenced. Nullptr if there is none found.
+            /**
+             * @brief   Get the kernel object reference by the handle.
+             * 
+             * @param   handle  The handle to extract kernel object from.
+             * @returns The kernel object referenced. Nullptr if there is none found.
             */
             kernel_obj_ptr get_object(uint32_t handle);
 
@@ -94,6 +104,32 @@ namespace eka2l1 {
 
             /*! \brief Get the last handle created. 0 if none left */
             std::uint32_t last_handle();
+
+            /**
+             * @brief   Check if the container currently has this object opened.
+             * 
+             * @param   obj         Pointer to this kernel object.
+             * @returns True if this container has given object.
+             */
+            bool has(kernel_obj_ptr obj);
+
+            /**
+             * @brief   Count the number of times this object appeared in the handle list.
+             * 
+             * This function counts the number of time a kernel object has been opened in this object container.
+             * Operates by iterating through all the container and searchs if this kernel object is associated with an index.
+             * 
+             * This function returns 0 if the object does not appear at all.
+             * 
+             * This function differs from the "has" function by searching through all index non-stop, while if "has" has found
+             * the object, it stops.
+             * 
+             * @param   obj     Pointer to the kernel object to be counted.
+             * @returns Number of times this object has appeared in this container
+             * 
+             * @see     has
+             */
+            std::uint32_t count(kernel_obj_ptr obj);
         };
     }
 }
--- src/emu/kernel/src/object_ix.cpp
+++ src/emu/kernel/src/object_ix.cpp
@@ -27,218 +27,240 @@
 
 #include <algorithm>
 
-namespace eka2l1 {
-    namespace kernel {
-        handle_inspect_info inspect_handle(std::uint32_t handle) {
-            handle_inspect_info info;
+namespace eka2l1::kernel {
+    handle_inspect_info inspect_handle(std::uint32_t handle) {
+        handle_inspect_info info;
 
-            if (handle >> 31 == 1) {
-                // Special
-                info.special = true;
-                info.special_type = static_cast<special_handle_type>(handle);
-            } else {
-                info.special = false;
-            }
-
-            if (((handle >> 30) & 0xf) == 1) {
-                info.handle_array_local = true;
-            } else {
-                info.handle_array_local = false;
-            }
-
-            // On emulator, use bit 29 to determine if it's a kernel handle
-            if (handle & (1 << 29)) {
-                info.handle_array_kernel = true;
-            } else {
-                info.handle_array_kernel = false;
-            }
-
-            info.no_close = handle & 0x8000;
-            info.object_ix_next_instance = (handle >> 16) & 0b0001111111111111;
-            info.object_ix_index = handle & 0x7FFF;
-
-            return info;
+        if (handle >> 31 == 1) {
+            // Special
+            info.special = true;
+            info.special_type = static_cast<special_handle_type>(handle);
+        } else {
+            info.special = false;
         }
 
-        std::uint32_t object_ix::make_handle(size_t index) {
-            std::uint32_t handle = 0;
-
-            handle |= next_instance << 16;
-            handle |= index;
-
-            if (owner == handle_array_owner::thread) {
-                // If handle array owner is thread, the 30th bit must be 1
-                handle |= 0x40000000;
-            }
-
-            if (owner == handle_array_owner::kernel) {
-                handle |= (1 << 29);
-            }
-
-            handles.push_back(handle);
-
-            return handle;
+        if (((handle >> 30) & 0xf) == 1) {
+            info.handle_array_local = true;
+        } else {
+            info.handle_array_local = false;
         }
 
-        std::uint32_t object_ix::add_object(kernel_obj_ptr obj) {
-            auto slot = std::find_if(objects.begin(), objects.end(),
-                [](object_ix_record record) { return record.free; });
+        // On emulator, use bit 29 to determine if it's a kernel handle
+        if (handle & (1 << 29)) {
+            info.handle_array_kernel = true;
+        } else {
+            info.handle_array_kernel = false;
+        }
 
-            if (slot != objects.end()) {
-                next_instance++;
-                std::uint32_t ret_handle = make_handle(slot - objects.begin());
+        info.no_close = handle & 0x8000;
+        info.object_ix_next_instance = (handle >> 16) & 0b0001111111111111;
+        info.object_ix_index = handle & 0x7FFF;
 
-                slot->associated_handle = ret_handle;
-                slot->free = false;
-                slot->object = obj;
+        return info;
+    }
 
-                obj->increase_access_count();
+    std::uint32_t object_ix::make_handle(size_t index) {
+        std::uint32_t handle = 0;
 
-                totals++;
-                return ret_handle;
-            }
+        handle |= next_instance << 16;
+        handle |= index;
 
+        if (owner == handle_array_owner::thread) {
+            // If handle array owner is thread, the 30th bit must be 1
+            handle |= 0x40000000;
+        }
+
+        if (owner == handle_array_owner::kernel) {
+            handle |= (1 << 29);
+        }
+
+        handles.push_back(handle);
+
+        return handle;
+    }
+
+    std::uint32_t object_ix::add_object(kernel_obj_ptr obj) {
+        auto slot = std::find_if(objects.begin(), objects.end(), [](const object_ix_record &record) {
+            return record.free;
+        });
+
+        if (slot != objects.end()) {
+            next_instance++;
+            std::uint32_t ret_handle = make_handle(slot - objects.begin());
+
+            slot->associated_handle = ret_handle;
+            slot->free = false;
+            slot->object = obj;
+
+            obj->increase_access_count();
+
+            totals++;
+            return ret_handle;
+        }
+
+        return INVALID_HANDLE;
+    }
+
+    std::uint32_t object_ix::last_handle() {
+        if (!handles.size()) {
+            return 0;
+        }
+
+        std::uint32_t last = handles.back();
+        handles.pop_back();
+
+        return last;
+    }
+
+    std::uint32_t object_ix::duplicate(std::uint32_t handle) {
+        handle_inspect_info info = inspect_handle(handle);
+
+        if (!info.handle_array_local && owner == handle_array_owner::process) {
             return INVALID_HANDLE;
         }
 
-        std::uint32_t object_ix::last_handle() {
-            if (!handles.size()) {
-                return 0;
-            }
+        kernel_obj_ptr obj = get_object(handle);
 
-            std::uint32_t last = handles.back();
-            handles.pop_back();
-
-            return last;
+        if (!obj) {
+            return INVALID_HANDLE;
         }
 
-        std::uint32_t object_ix::duplicate(std::uint32_t handle) {
-            handle_inspect_info info = inspect_handle(handle);
+        return add_object(obj);
+    }
 
-            if (!info.handle_array_local && owner == handle_array_owner::process) {
-                return INVALID_HANDLE;
+    kernel_obj_ptr object_ix::get_object(std::uint32_t handle) {
+        handle_inspect_info info = inspect_handle(handle);
+
+        if (info.object_ix_index < objects.size()) {
+            if (objects[info.object_ix_index].free) {
+                return nullptr;
             }
 
-            kernel_obj_ptr obj = get_object(handle);
+            return objects[info.object_ix_index].object;
+        }
+
+        LOG_WARN("Can't find object with handle: 0x{:x}", handle);
+
+        return nullptr;
+    }
+
+    int object_ix::close(std::uint32_t handle) {
+        handle_inspect_info info = inspect_handle(handle);
+        int ret_value = 0;
+
+        if (info.object_ix_index < objects.size()) {
+            kernel_obj_ptr obj = objects[info.object_ix_index].object;
 
             if (!obj) {
-                return INVALID_HANDLE;
+                return -1;
             }
 
-            return add_object(obj);
-        }
+            obj->decrease_access_count();
+            obj->close();
 
-        kernel_obj_ptr object_ix::get_object(std::uint32_t handle) {
-            handle_inspect_info info = inspect_handle(handle);
+            totals--;
 
-            if (info.object_ix_index < objects.size()) {
-                if (objects[info.object_ix_index].free) {
-                    return nullptr;
-                }
+            if (obj->get_access_count() <= 0 && obj->get_object_type() != object_type::process && obj->get_object_type() != object_type::thread) {
+                if (obj->get_object_type() == object_type::chunk) {
+                    chunk_ptr c = reinterpret_cast<kernel::chunk *>(obj);
 
-                return objects[info.object_ix_index].object;
-            }
-
-            LOG_WARN("Can't find object with handle: 0x{:x}", handle);
-
-            return nullptr;
-        }
-
-        int object_ix::close(std::uint32_t handle) {
-            handle_inspect_info info = inspect_handle(handle);
-            int ret_value = 0;
-
-            if (info.object_ix_index < objects.size()) {
-                kernel_obj_ptr obj = objects[info.object_ix_index].object;
-
-                if (!obj) {
-                    return -1;
-                }
-
-                obj->decrease_access_count();
-                obj->close();
-
-                totals--;
-
-                if (obj->get_access_count() <= 0 && obj->get_object_type() != object_type::process && obj->get_object_type() != object_type::thread) {
-                    if (obj->get_object_type() == object_type::chunk) {
-                        chunk_ptr c = reinterpret_cast<kernel::chunk *>(obj);
-
-                        // This is a force hack signaling the closing one is chunk heap, which means the
-                        // thread is in destruction, and detach needed
-                        if (c->is_chunk_heap()) {
-                            ret_value = 1;
-                        }
+                    // This is a force hack signaling the closing one is chunk heap, which means the
+                    // thread is in destruction, and detach needed
+                    if (c->is_chunk_heap()) {
+                        ret_value = 1;
                     }
-
-                    kern->destroy(obj);
                 }
 
-                objects[info.object_ix_index].free = true;
-
-                return ret_value;
+                kern->destroy(obj);
             }
 
-            return -1;
+            objects[info.object_ix_index].free = true;
+            objects[info.object_ix_index].object = nullptr;
+
+            return ret_value;
         }
 
-        object_ix::object_ix(kernel_system *kern, handle_array_owner owner)
-            : kern(kern)
-            , owner(owner)
-            , next_instance(0)
-            , uid(kern->next_uid())
-            , totals(0) {}
+        return -1;
+    }
 
-        void object_ix::do_state(common::chunkyseri &seri) {
-            auto s = seri.section("ObjectIx", 1);
-
-            if (!s) {
-                return;
+    bool object_ix::has(kernel_obj_ptr obj) {
+        for (const auto &index: objects) {
+            if ((index.object == obj) && (index.free == false)) {
+                return true;
             }
+        }
 
-            seri.absorb(uid);
-            seri.absorb(next_instance);
-            seri.absorb(owner);
+        return false;
+    }
 
-            std::stack<std::uint16_t> slot_used;
-            std::uint32_t slot_count = 0;
+    std::uint32_t object_ix::count(kernel_obj_ptr obj) {
+        std::uint32_t so_far = 0;
+
+        for (const auto &index: objects) {
+            if ((index.free == false) && (index.object == obj)) {
+                so_far++;
+            }
+        }
+
+        return so_far;
+    }
+
+    object_ix::object_ix(kernel_system *kern, handle_array_owner owner)
+        : kern(kern)
+        , owner(owner)
+        , next_instance(0)
+        , uid(kern->next_uid())
+        , totals(0) {}
+
+    void object_ix::do_state(common::chunkyseri &seri) {
+        auto s = seri.section("ObjectIx", 1);
+
+        if (!s) {
+            return;
+        }
+
+        seri.absorb(uid);
+        seri.absorb(next_instance);
+        seri.absorb(owner);
+
+        std::stack<std::uint16_t> slot_used;
+        std::uint32_t slot_count = 0;
+
+        if (seri.get_seri_mode() == common::SERI_MODE_WRITE) {
+            for (std::size_t i = 0; i < objects.size(); i++) {
+                if (!objects[i].free) {
+                    slot_count++;
+                    slot_used.push(static_cast<std::uint16_t>(i));
+                }
+            }
+        }
+
+        seri.absorb(slot_count);
+
+        std::uint32_t next_slot_use = 0;
+
+        for (std::uint32_t i = 0; i < slot_count; i++) {
+            std::uint32_t obj_id = 0;
 
             if (seri.get_seri_mode() == common::SERI_MODE_WRITE) {
-                for (std::size_t i = 0; i < objects.size(); i++) {
-                    if (!objects[i].free) {
-                        slot_count++;
-                        slot_used.push(static_cast<std::uint16_t>(i));
-                    }
-                }
+                next_slot_use = slot_used.top();
+                slot_used.pop();
+
+                obj_id = objects[next_slot_use].object->unique_id();
             }
 
-            seri.absorb(slot_count);
+            seri.absorb(next_slot_use);
+            seri.absorb(obj_id);
+            seri.absorb(objects[next_slot_use].associated_handle);
 
-            std::uint32_t next_slot_use = 0;
-
-            for (std::uint32_t i = 0; i < slot_count; i++) {
-                std::uint32_t obj_id = 0;
-
-                if (seri.get_seri_mode() == common::SERI_MODE_WRITE) {
-                    next_slot_use = slot_used.top();
-                    slot_used.pop();
-
-                    obj_id = objects[next_slot_use].object->unique_id();
-                }
-
-                seri.absorb(next_slot_use);
-                seri.absorb(obj_id);
-                seri.absorb(objects[next_slot_use].associated_handle);
-
-                if (seri.get_seri_mode() == common::SERI_MODE_READ) {
-                    // TODO
-                    //objects[next_slot_use].object = kern->get_kernel_obj_raw(obj_id);
-                    objects[next_slot_use].free = false;
-                }
+            if (seri.get_seri_mode() == common::SERI_MODE_READ) {
+                // TODO
+                //objects[next_slot_use].object = kern->get_kernel_obj_raw(obj_id);
+                objects[next_slot_use].free = false;
             }
-
-            // Hey, we need to save last thread handle too
-            seri.absorb_container(handles);
         }
+
+        // Hey, we need to save last thread handle too
+        seri.absorb_container(handles);
     }
 }
