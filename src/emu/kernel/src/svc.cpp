88664c8262a3a07542f53691e93ba492cf5a0d15
aikawa <l51492003231@163.com>
2020-08-25 18:21:57 +0700

    kernel/svc: Add mutex_wait/mutex_signal for s60v1

--- src/emu/kernel/src/svc.cpp
+++ src/emu/kernel/src/svc.cpp
@@ -3634,6 +3634,8 @@ namespace eka2l1::epoc {
         BRIDGE_REGISTER(0x01, chunk_base),
         BRIDGE_REGISTER(0x02, chunk_size),
         BRIDGE_REGISTER(0x03, chunk_max_size),
+        BRIDGE_REGISTER(0x19, mutex_wait),
+        BRIDGE_REGISTER(0x1A, mutex_signal),
         BRIDGE_REGISTER(0x2A, semaphore_wait),
         BRIDGE_REGISTER(0x32, thread_id),
         BRIDGE_REGISTER(0x4D, wait_for_any_request),
