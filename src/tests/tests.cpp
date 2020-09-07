0531b8f599345cd8a27cf84621132ef23d9e9782
pent0 <fewdspuckrito@gmail.com>
2020-09-07 09:22:35 +0700

    tests: Add license header

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2017 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <common/log.h>
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
