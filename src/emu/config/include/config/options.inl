43d1ebbc6f49f4446556eab006dca6d523aab1b5
aikawa <l51492003231@163.com>
2020-10-14 18:02:57 +0700

    config: Unify config serialization in external file

---
+++ src/emu/config/include/config/options.inl
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2019 EKA2L1 Team.
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPTION
+#define OPTION(name, variable, default_var)
+#endif
+
+OPTION(ui-scale, ui_scale, 1.0f)
+OPTION(bkg-alpha, bkg_transparency, 129)
+OPTION(bkg-path, bkg_path, "")
+OPTION(font, font_path, "")
+OPTION(log-read, log_read, false)
+OPTION(log-write, log_write, false)
+OPTION(log-ipc, log_ipc, false)
+OPTION(log-svc, log_svc, false)
+OPTION(log-passed, log_passed, false)
+OPTION(log-exports, log_exports, false)
+OPTION(cpu, cpu_backend, 0)
+OPTION(device, device, 0)
+OPTION(language, language, -1)
+OPTION(emulator-language, emulator_language, -1)
+OPTION(enable-gdb-stub, enable_gdbstub, false)
+OPTION(data-storage, storage, "")
+OPTION(gdb-port, gdb_port, 24689)
+OPTION(enable-srv-ecom, enable_srv_ecom, true)
+OPTION(enable-srv-cenrep, enable_srv_cenrep, true)
+OPTION(enable-srv-backup, enable_srv_backup, true)
+OPTION(enable-srv-install, enable_srv_install, true)
+OPTION(enable-srv-rights, enable_srv_rights, true)
+OPTION(enable-srv-sa, enable_srv_sa, true)
+OPTION(enable-srv-drm, enable_srv_drm, true)
+OPTION(enable-srv-eikappui, enable_srv_eikapp_ui, true)
+OPTION(enable-srv-akn-icon, enable_srv_akn_icon, true)
+OPTION(enable-srv-akn-skin, enable_srv_akn_skin, true)
+OPTION(enable-srv-cdl, enable_srv_cdl, true)
+OPTION(enable-srv-socket, enable_srv_socket, false)
+OPTION(fbs-enable-compression-queue, fbs_enable_compression_queue, false)
+OPTION(accurate-ipc-timing, accurate_ipc_timing, false)
+OPTION(enable-btrace, enable_btrace, false)
+OPTION(stop-warn-touchscreen-disabled, stop_warn_touch_disabled, false)
+OPTION(dump-imb-range-code, dump_imb_range_code, false)
+OPTION(hide-mouse-in-screen-space, hide_mouse_in_screen_space, false)
+OPTION(enable-nearest-neighbor-filter, nearest_neighbor_filtering, false)
+OPTION(integer-scaling, integer_scaling, true)
+OPTION(cpu-load-save, cpu_load_save, true)
+
+#ifdef OPTION
+#undef OPTION
+#endif
--- src/emu/config/src/config.cpp
+++ src/emu/config/src/config.cpp
@@ -77,44 +77,9 @@ namespace eka2l1::config {
         YAML::Emitter emitter;
         emitter << YAML::BeginMap;
 
-        config_file_emit_single(emitter, "ui-scale", ui_scale);
-        config_file_emit_single(emitter, "bkg-alpha", bkg_transparency);
-        config_file_emit_single(emitter, "bkg-path", bkg_path);
-        config_file_emit_single(emitter, "font", font_path);
-        config_file_emit_single(emitter, "log-read", log_read);
-        config_file_emit_single(emitter, "log-write", log_write);
-        config_file_emit_single(emitter, "log-ipc", log_ipc);
-        config_file_emit_single(emitter, "log-svc", log_svc);
-        config_file_emit_single(emitter, "log-passed", log_passed);
-        config_file_emit_single(emitter, "log-exports", log_exports);
-        config_file_emit_single(emitter, "cpu", cpu_backend);
-        config_file_emit_single(emitter, "device", device);
-        config_file_emit_single(emitter, "language", language);
-        config_file_emit_single(emitter, "emulator-language", emulator_language);
-        config_file_emit_single(emitter, "enable-gdb-stub", enable_gdbstub);
-        config_file_emit_single(emitter, "data-storage", storage);
-        config_file_emit_single(emitter, "gdb-port", gdb_port);
-        config_file_emit_single(emitter, "enable-srv-ecom", enable_srv_ecom);
-        config_file_emit_single(emitter, "enable-srv-cenrep", enable_srv_cenrep);
-        config_file_emit_single(emitter, "enable-srv-backup", enable_srv_backup);
-        config_file_emit_single(emitter, "enable-srv-install", enable_srv_install);
-        config_file_emit_single(emitter, "enable-srv-rights", enable_srv_rights);
-        config_file_emit_single(emitter, "enable-srv-sa", enable_srv_sa);
-        config_file_emit_single(emitter, "enable-srv-drm", enable_srv_drm);
-        config_file_emit_single(emitter, "enable-srv-eikappui", enable_srv_eikapp_ui);
-        config_file_emit_single(emitter, "enable-srv-akn-icon", enable_srv_akn_icon);
-        config_file_emit_single(emitter, "enable-srv-akn-skin", enable_srv_akn_skin);
-        config_file_emit_single(emitter, "enable-srv-cdl", enable_srv_cdl);
-        config_file_emit_single(emitter, "enable-srv-socket", enable_srv_socket);
-        config_file_emit_single(emitter, "fbs-enable-compression-queue", fbs_enable_compression_queue);
-        config_file_emit_single(emitter, "accurate-ipc-timing", accurate_ipc_timing);
-        config_file_emit_single(emitter, "enable-btrace", enable_btrace);
-        config_file_emit_single(emitter, "stop-warn-touchscreen-disabled", stop_warn_touch_disabled);
-        config_file_emit_single(emitter, "dump-imb-range-code", dump_imb_range_code);
-        config_file_emit_single(emitter, "hide-mouse-in-screen-space", hide_mouse_in_screen_space);
-        config_file_emit_single(emitter, "enable-nearest-neighbor-filter", nearest_neighbor_filtering);
-        config_file_emit_single(emitter, "integer-scaling", integer_scaling);
-        config_file_emit_single(emitter, "cpu-load-save", cpu_load_save);
+        #define OPTION(name, variable, default) config_file_emit_single(emitter, #name, variable);
+        #include <config/options.inl>
+        #undef OPTION
 
         emitter << YAML::EndMap;
 
@@ -144,44 +109,9 @@ namespace eka2l1::config {
             return;
         }
 
-        get_yaml_value(node, "ui-scale", &ui_scale, 1.0f);
-        get_yaml_value(node, "bkg-alpha", &bkg_transparency, 129);
-        get_yaml_value(node, "bkg-path", &bkg_path, "");
-        get_yaml_value(node, "font", &font_path, "");
-        get_yaml_value(node, "log-read", &log_read, false);
-        get_yaml_value(node, "log-write", &log_write, false);
-        get_yaml_value(node, "log-ipc", &log_ipc, false);
-        get_yaml_value(node, "log-svc", &log_svc, false);
-        get_yaml_value(node, "log-passed", &log_passed, false);
-        get_yaml_value(node, "log-exports", &log_exports, false);
-        get_yaml_value(node, "cpu", &cpu_backend, 0);
-        get_yaml_value(node, "device", &device, 0);
-        get_yaml_value(node, "language", &language, -1);
-        get_yaml_value(node, "emulator-language", &emulator_language, -1);
-        get_yaml_value(node, "enable-gdb-stub", &enable_gdbstub, false);
-        get_yaml_value(node, "data-storage", &storage, "");
-        get_yaml_value(node, "gdb-port", &gdb_port, 24689);
-        get_yaml_value(node, "enable-srv-ecom", &enable_srv_ecom, true);
-        get_yaml_value(node, "enable-srv-cenrep", &enable_srv_cenrep, true);
-        get_yaml_value(node, "enable-srv-backup", &enable_srv_backup, true);
-        get_yaml_value(node, "enable-srv-install", &enable_srv_install, true);
-        get_yaml_value(node, "enable-srv-rights", &enable_srv_rights, true);
-        get_yaml_value(node, "enable-srv-sa", &enable_srv_sa, true);
-        get_yaml_value(node, "enable-srv-drm", &enable_srv_drm, true);
-        get_yaml_value(node, "enable-srv-eikappui", &enable_srv_eikapp_ui, true);
-        get_yaml_value(node, "enable-srv-akn-icon", &enable_srv_akn_icon, true);
-        get_yaml_value(node, "enable-srv-akn-skin", &enable_srv_akn_skin, true);
-        get_yaml_value(node, "enable-srv-cdl", &enable_srv_cdl, true);
-        get_yaml_value(node, "enable-srv-socket", &enable_srv_socket, false);
-        get_yaml_value(node, "fbs-enable-compression-queue", &fbs_enable_compression_queue, false);
-        get_yaml_value(node, "accurate-ipc-timing", &accurate_ipc_timing, false);
-        get_yaml_value(node, "enable-btrace", &enable_btrace, false);
-        get_yaml_value(node, "stop-warn-touchscreen-disabled", &stop_warn_touch_disabled, false);
-        get_yaml_value(node, "dump-imb-range-code", &dump_imb_range_code, false);
-        get_yaml_value(node, "hide-mouse-in-screen-space", &hide_mouse_in_screen_space, false);
-        get_yaml_value(node, "enable-nearest-neighbor-filter", &nearest_neighbor_filtering, false);
-        get_yaml_value(node, "integer-scaling", &integer_scaling, true);
-        get_yaml_value(node, "cpu-load-save", &cpu_load_save, true);
+        #define OPTION(name, variable, default_value) get_yaml_value(node, #name, &variable, default_value);
+        #include <config/options.inl>
+        #undef OPTION
 
         YAML::Node keybind_node;
         try {
