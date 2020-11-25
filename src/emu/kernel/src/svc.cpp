99735ff6aaf55facb33441a145227cc70a65b759
Nikita Shakarun <nikita36078@gmail.com>
2020-11-25 21:41:20 +0300

    kernel/svc: Add MathRand for s60v3

--- src/emu/kernel/src/svc.cpp
+++ src/emu/kernel/src/svc.cpp
@@ -4443,6 +4443,7 @@ namespace eka2l1::epoc {
         BRIDGE_REGISTER(0x02, chunk_size),
         BRIDGE_REGISTER(0x03, chunk_max_size),
         BRIDGE_REGISTER(0x05, tick_count),
+        BRIDGE_REGISTER(0x0B, math_rand),
         BRIDGE_REGISTER(0x0C, imb_range),
         BRIDGE_REGISTER(0x0E, library_lookup),
         BRIDGE_REGISTER(0x11, mutex_wait),
