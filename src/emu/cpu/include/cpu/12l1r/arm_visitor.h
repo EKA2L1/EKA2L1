a908dd0a9f7435bc7f7d7ca627e6fabaecd75c27
mangren <l51492003231@163.com>
2021-01-08 10:13:03 +0700

    cpu/12l1r: Implement ORR(imm/reg/rsr)

--- src/emu/cpu/include/cpu/12l1r/arm_visitor.h
+++ src/emu/cpu/include/cpu/12l1r/arm_visitor.h
@@ -45,6 +45,11 @@ namespace eka2l1::arm::r12l1 {
             common::armgen::shift_type shift, reg_index m);
         bool arm_BIC_rsr(common::cc_flags cond, bool S, reg_index n, reg_index d, reg_index s,
             common::armgen::shift_type shift, reg_index m);
+        bool arm_ORR_imm(common::cc_flags cond, bool S, reg_index n, reg_index d, int rotate, std::uint8_t imm8);
+        bool arm_ORR_reg(common::cc_flags cond, bool S, reg_index n, reg_index d, std::uint8_t imm5,
+            common::armgen::shift_type shift, reg_index m);
+        bool arm_ORR_rsr(common::cc_flags cond, bool S, reg_index n, reg_index d, reg_index s,
+            common::armgen::shift_type shift, reg_index m);
         bool arm_CMP_imm(common::cc_flags cond, reg_index n, int rotate, std::uint8_t imm8);
         bool arm_CMP_reg(common::cc_flags cond, reg_index n, std::uint8_t imm5, common::armgen::shift_type shift, reg_index m);
 
