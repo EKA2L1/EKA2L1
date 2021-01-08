c2d4b3173ff61870a39dea898a2b6fbf1552d128
mangren <l51492003231@163.com>
2021-01-08 22:58:04 +0700

    cpu/12l1r: Implement thumb16 ADD/SUB
    ADD/SUB with SP all
    ADD SUB with imm all (unverified)

--- src/emu/cpu/include/cpu/12l1r/thumb_visitor.h
+++ src/emu/cpu/include/cpu/12l1r/thumb_visitor.h
@@ -35,6 +35,13 @@ namespace eka2l1::arm::r12l1 {
         // Data processing
         bool thumb16_MOV_imm(reg_index d, std::uint8_t imm8);
         bool thumb16_CMP_imm(reg_index n, std::uint8_t imm8);
+        bool thumb16_ADD_imm_t1(std::uint8_t imm3, reg_index n, reg_index d);
+        bool thumb16_ADD_imm_t2(reg_index d_n, std::uint8_t imm8);
+        bool thumb16_ADD_sp_t1(reg_index d, std::uint8_t imm8);
+        bool thumb16_ADD_sp_t2(std::uint8_t imm7);
+        bool thumb16_SUB_imm_t1(std::uint8_t imm3, reg_index n, reg_index d);
+        bool thumb16_SUB_imm_t2(reg_index d_n, std::uint8_t imm8);
+        bool thumb16_SUB_sp(std::uint8_t imm7);
 
         bool thumb16_PUSH(bool m, reg_list reg_list);
         bool thumb16_POP(bool p, reg_list reg_list);
--- src/emu/cpu/src/12l1r/translate/data_processing.cpp
+++ src/emu/cpu/src/12l1r/translate/data_processing.cpp
@@ -574,6 +574,86 @@ namespace eka2l1::arm::r12l1 {
         return true;
     }
 
+    bool thumb_translate_visitor::thumb16_ADD_imm_t1(std::uint8_t imm3, reg_index n, reg_index d) {
+        common::armgen::arm_reg dest_real = reg_index_to_gpr(d);
+        common::armgen::arm_reg op1_real = reg_index_to_gpr(n);
+        common::armgen::operand2 op2(imm3, 0);
+
+        const common::armgen::arm_reg dest_mapped = reg_supplier_.map(dest_real, ALLOCATE_FLAG_DIRTY);
+        const common::armgen::arm_reg op1_mapped = reg_supplier_.map(op1_real, 0);
+
+        big_block_->ADDS(dest_mapped, op1_mapped, op2);
+        cpsr_nzcv_changed();
+
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_ADD_imm_t2(reg_index d_n, std::uint8_t imm8) {
+        common::armgen::arm_reg dest_and_op1_real = reg_index_to_gpr(d_n);
+        common::armgen::operand2 op2(imm8, 0);
+
+        const common::armgen::arm_reg dest_and_op1_mapped = reg_supplier_.map(dest_and_op1_real,
+            ALLOCATE_FLAG_DIRTY);
+
+        big_block_->ADDS(dest_and_op1_mapped, dest_and_op1_mapped, op2);
+        cpsr_nzcv_changed();
+
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_ADD_sp_t1(reg_index d, std::uint8_t imm8) {
+        common::armgen::arm_reg dest_real = reg_index_to_gpr(d);
+
+        common::armgen::arm_reg sp_mapped = reg_supplier_.map(common::armgen::R13, 0);
+        common::armgen::arm_reg dest_mapped = reg_supplier_.map(dest_real, ALLOCATE_FLAG_DIRTY);
+
+        big_block_->ADDI2R(dest_mapped, sp_mapped, imm8 << 2, ALWAYS_SCRATCH1);
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_ADD_sp_t2(std::uint8_t imm7) {
+        const common::armgen::arm_reg sp_mapped = reg_supplier_.map(common::armgen::R13,
+            ALLOCATE_FLAG_DIRTY);
+
+        big_block_->ADDI2R(sp_mapped, sp_mapped, imm7 << 2, ALWAYS_SCRATCH1);
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_SUB_imm_t1(std::uint8_t imm3, reg_index n, reg_index d) {
+        common::armgen::arm_reg dest_real = reg_index_to_gpr(d);
+        common::armgen::arm_reg op1_real = reg_index_to_gpr(n);
+        common::armgen::operand2 op2(imm3, 0);
+
+        const common::armgen::arm_reg dest_mapped = reg_supplier_.map(dest_real, ALLOCATE_FLAG_DIRTY);
+        const common::armgen::arm_reg op1_mapped = reg_supplier_.map(op1_real, 0);
+
+        big_block_->SUBS(dest_mapped, op1_mapped, op2);
+        cpsr_nzcv_changed();
+
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_SUB_imm_t2(reg_index d_n, std::uint8_t imm8) {
+        common::armgen::arm_reg dest_and_op1_real = reg_index_to_gpr(d_n);
+        common::armgen::operand2 op2(imm8, 0);
+
+        const common::armgen::arm_reg dest_and_op1_mapped = reg_supplier_.map(dest_and_op1_real,
+            ALLOCATE_FLAG_DIRTY);
+
+        big_block_->SUBS(dest_and_op1_mapped, dest_and_op1_mapped, op2);
+        cpsr_nzcv_changed();
+
+        return true;
+    }
+
+    bool thumb_translate_visitor::thumb16_SUB_sp(std::uint8_t imm7) {
+        const common::armgen::arm_reg sp_mapped = reg_supplier_.map(common::armgen::R13,
+            ALLOCATE_FLAG_DIRTY);
+
+        big_block_->SUBI2R(sp_mapped, sp_mapped, imm7 << 2, ALWAYS_SCRATCH1);
+        return true;
+    }
+
     bool thumb_translate_visitor::thumb16_CMP_imm(reg_index n, std::uint8_t imm8) {
         common::armgen::arm_reg lhs_real = reg_index_to_gpr(n);
         common::armgen::operand2 rhs(imm8, 0);
