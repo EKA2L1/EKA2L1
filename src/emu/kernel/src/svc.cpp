48ea090a7b515645ba2cefe9b7a713d9ea76c788
pent0 <fewdspuckrito@gmail.com>
2021-04-06 20:38:39 +0700

    kernel: Add session_create_from_handle for s60v3

--- src/emu/kernel/src/svc.cpp
+++ src/emu/kernel/src/svc.cpp
@@ -4779,6 +4779,7 @@ namespace eka2l1::epoc {
         BRIDGE_REGISTER(0x7C, thread_process),
         BRIDGE_REGISTER(0x7D, server_create),
         BRIDGE_REGISTER(0x7E, session_create),
+        BRIDGE_REGISTER(0x7F, session_create_from_handle),
         BRIDGE_REGISTER(0x83, timer_create),
         BRIDGE_REGISTER(0x85, after), // Actually AfterHighRes
         BRIDGE_REGISTER(0x86, change_notifier_create),
