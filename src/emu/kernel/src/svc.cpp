b7d165e86f4ec20fe6706d18cf8e7121b2348da4
pent0 <fewdspuckrito@gmail.com>
2021-06-08 11:46:07 +0700

    kernel/svc: Add process_logon_cancel for s60v3

--- src/emu/kernel/src/svc.cpp
+++ src/emu/kernel/src/svc.cpp
@@ -5448,6 +5448,7 @@ namespace eka2l1::epoc {
         BRIDGE_REGISTER(0x77, thread_rename),
         BRIDGE_REGISTER(0x78, process_rename),
         BRIDGE_REGISTER(0x7A, process_logon),
+        BRIDGE_REGISTER(0x7B, process_logon_cancel),
         BRIDGE_REGISTER(0x7C, thread_process),
         BRIDGE_REGISTER(0x7D, server_create),
         BRIDGE_REGISTER(0x7E, session_create),
