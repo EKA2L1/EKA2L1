ce3d0a95dc793a8d6d848cf9faae88d17494823e
pent0 <fewdspuckrito@gmail.com>
2021-07-06 00:48:02 +0700

    qt: Add device install, package install cancel, package install drag and drop and mount mmc, recent mount

--- src/emu/qt/include/qt/mainwindow.h
+++ src/emu/qt/include/qt/mainwindow.h
@@ -10,6 +10,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class main_window; }
 QT_END_NAMESPACE
 
+static constexpr std::size_t MAX_RECENT_ENTRIES = 5;
+
 namespace eka2l1 {
     class system;
     class window_server;
@@ -35,13 +37,28 @@ private:
 
     QProgressDialog *current_package_install_dialog_;
 
+    QMenu *recent_mount_folder_menu_;
+    QAction *recent_mount_folder_actions_[MAX_RECENT_ENTRIES];
+    QAction *recent_mount_clear_;
+
     void setup_screen_draw();
+    void setup_app_list();
+    void setup_package_installer_ui_hooks();
+    void refresh_recent_mounts();
+    void mount_game_card_dump(QString path);
+    void spawn_package_install_camper(QString package_install_path);
 
 private slots:
     void on_about_triggered();
     void on_package_install_clicked();
+    void on_device_install_clicked();
     void on_package_install_progress_change(const std::size_t now, const std::size_t total);
     bool on_package_install_text_ask(const char *text, const bool one_button);
+    void on_new_device_added();
+    void on_mount_card_clicked();
+    void on_recent_mount_clear_clicked();
+    void on_recent_mount_card_folder_clicked();
+
     int on_package_install_language_choose(const int *languages, const int language_count);
 
     void on_app_clicked(QListWidgetItem *item);
@@ -56,6 +73,8 @@ public:
     ~main_window();
 
     void resizeEvent(QResizeEvent *event) override;
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
 
     display_widget *render_window() {
         return displayer_;
