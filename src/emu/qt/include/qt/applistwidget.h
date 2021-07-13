d17b350cd53651ca0d9fb853c1eaf4493b02fbc7
pent0 <fewdspuckrito@gmail.com>
2021-07-13 23:08:28 +0700

    qt/applist: Add search bar and render of SVG/MBM morden icons

--- src/emu/qt/include/qt/applistwidget.h
+++ src/emu/qt/include/qt/applistwidget.h
@@ -2,10 +2,14 @@
 #define APPLISTWIDGET_H
 
 #include <QListWidget>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QGridLayout>
 
 namespace eka2l1 {
     class applist_server;
     class fbs_server;
+    class io_system;
 
     struct apa_app_registry;
 }
@@ -17,19 +21,57 @@ public:
     applist_widget_item(const QIcon &icon, const QString &name, int registry_index, QListWidget *listview);
 };
 
-class applist_widget : public QListWidget
-{
-private:
-    eka2l1::applist_server *lister_;
-    eka2l1::fbs_server *fbss_;
+class applist_search_bar: public QWidget {
+    Q_OBJECT;
 
-    void add_registeration_item(eka2l1::apa_app_registry &reg, const int index);
-    eka2l1::apa_app_registry *get_registry_from_widget_item(QListWidgetItem *item);
+private:
+    QLabel *search_label_;
+    QLineEdit *search_line_edit_;
+
+    QHBoxLayout *search_layout_;
+
+private slots:
+    void on_search_bar_content_changed(QString content);
+
+signals:
+    void new_search(QString content);
 
 public:
-    explicit applist_widget(QWidget *parent, eka2l1::applist_server *lister, eka2l1::fbs_server *fbss);
-    bool launch_from_widget_item(QListWidgetItem *item);
+    applist_search_bar(QWidget *parent = nullptr);
+    ~applist_search_bar();
+};
 
+class applist_widget : public QWidget
+{
+    Q_OBJECT;
+
+private:
+    applist_search_bar *search_bar_;
+    QListWidget *list_widget_;
+    QGridLayout *layout_;
+
+    eka2l1::applist_server *lister_;
+    eka2l1::fbs_server *fbss_;
+    eka2l1::io_system *io_;
+
+    void add_registeration_item(eka2l1::apa_app_registry &reg, const int index);
+    eka2l1::apa_app_registry *get_registry_from_widget_item(applist_widget_item *item);
+
+    void hide_all();
+    void show_all();
+    void on_search_content_changed(QString content);
+
+private slots:
+    void on_list_widget_item_clicked(QListWidgetItem *item);
+
+signals:
+    void app_launch(applist_widget_item *item);
+
+public:
+    explicit applist_widget(QWidget *parent, eka2l1::applist_server *lister, eka2l1::fbs_server *fbss, eka2l1::io_system *io);
+    ~applist_widget();
+
+    bool launch_from_widget_item(applist_widget_item *item);
     void reload_whole_list();
 };
 
