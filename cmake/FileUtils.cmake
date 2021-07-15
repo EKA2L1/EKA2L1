150f7b6ca3ab05f2b498deba08994d411f4d1c3b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-15 19:08:07 +0800

    Delete cmake directory
    Counter terrorism act

--- cmake/FileUtils.cmake
+++
@@ -1,6 +0,0 @@
-macro(add_assets DETARGET PATH DESTPATH)
-    add_custom_command(
-                TARGET ${DETARGET}
-                POST_BUILD
-                COMMAND ${CMAKE_COMMAND} -E copy_directory "${PATH}" "$<TARGET_FILE_DIR:${DETARGET}>/${DESTPATH}/")
-endmacro()
