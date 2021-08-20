08c38b3421860bfbccff75a15c64c98e8d5c76c7
pent0 <25717050+pent0@users.noreply.github.com>
2021-08-21 01:13:48 +0700

    New translations eka2l1.ts (Portuguese, Brazilian)

--- src/emu/qt/translations/eka2l1_pt_BR.ts
+++ src/emu/qt/translations/eka2l1_pt_BR.ts
@@ -668,12 +668,12 @@
     <message>
       <location filename="../src/settings_dialog.ui" line="349"/>
       <source>Background color</source>
-      <translation type="unfinished">Background color</translation>
+      <translation>Cor do fundo</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="362"/>
       <source>Pick</source>
-      <translation type="unfinished">Pick</translation>
+      <translation>Escolher</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="372"/>
@@ -688,12 +688,12 @@
     <message>
       <location filename="../src/settings_dialog.ui" line="386"/>
       <source>By default, the emulator only allows resizing the emulator display to the minimum of the phone screen&apos;s size, multiply with the system display scale, to suit most users&apos;s eyes</source>
-      <translation type="unfinished">By default, the emulator only allows resizing the emulator display to the minimum of the phone screen&apos;s size, multiply with the system display scale, to suit most users&apos;s eyes</translation>
+      <translation>Por padrão, o emulador só permite o redimensionamento da exibição do emulador até o mínimo da tela do telefone&apos;s, multiplicar com a escala de exibição do sistema, para atender a maioria dos usuários&apos;s olhos</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="389"/>
       <source>Allow resizing display to phone screen&apos;s true size in pixels</source>
-      <translation type="unfinished">Allow resizing display to phone screen&apos;s true size in pixels</translation>
+      <translation>Permitir redimensionamento da tela do telefone&apos;tamanho verdadeiro em pixels</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="401"/>
@@ -1148,7 +1148,7 @@
     <message>
       <location filename="../src/settings_dialog.cpp" line="1124"/>
       <source>Pick the screen background color</source>
-      <translation type="unfinished">Pick the screen background color</translation>
+      <translation>Selecionar cor de fundo da tela</translation>
     </message>
   </context>
 </TS>
