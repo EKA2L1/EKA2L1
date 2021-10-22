52c7ec365d0788c0ce381fa0ca5181560c0bca6e
Foma Nikitin <l51492003231@163.com>
2021-10-22 14:39:39 +0700

    scripting: Add write memory function and change behaviour
    Read from current process now

--- src/emu/scripting/lua/symemu/mem.lua
+++ src/emu/scripting/lua/symemu/mem.lua
@@ -9,6 +9,11 @@ ffi.cdef([[
     uint16_t symemu_mem_read_word(const uint32_t addr);
     uint32_t symemu_mem_read_dword(const uint32_t addr);
     uint64_t symemu_mem_read_qword(const uint32_t addr);
+
+    int32_t symemu_mem_write_byte(const uint32_t addr, const uint8_t data);
+    int32_t symemu_mem_write_word(const uint32_t addr, const uint16_t data);
+    int32_t symemu_mem_write_dword(const uint32_t addr, const uint32_t data);
+    int32_t symemu_mem_write_qword(const uint32_t addr, const uint64_t data);
 ]])
 
 function mem.readByte(addr)
@@ -25,6 +30,22 @@ end
 
 function mem.readQword(addr)
     return ffi.C.symemu_mem_read_qword(addr)
+end
+
+function mem.writeByte(addr, data)
+    return (ffi.C.symemu_mem_write_byte(addr, data) == 0) and false or true
+end
+
+function mem.readWord(addr, data)
+    return (ffi.C.symemu_mem_write_word(addr, data) == 0) and false or true
+end
+
+function mem.readDword(addr, data)
+    return (ffi.C.symemu_mem_write_dword(addr, data) == 0) and false or true
+end
+
+function mem.readQword(addr, data)
+    return (ffi.C.symemu_mem_write_qword(addr, data) == 0) and false or true
 end
 
 return mem
