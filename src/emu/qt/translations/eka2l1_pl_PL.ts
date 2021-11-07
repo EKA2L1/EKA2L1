1c81655a16adfc9a2005093939957a4e38e4c234
pent0 <25717050+pent0@users.noreply.github.com>
2021-11-08 01:12:21 +0700

    New translations eka2l1.ts (Polish)

--- src/emu/qt/translations/eka2l1_pl_PL.ts
+++ src/emu/qt/translations/eka2l1_pl_PL.ts
@@ -904,12 +904,12 @@
     <message>
       <location filename="../src/settings_dialog.ui" line="798"/>
       <source>The name that will be displayed in emulated applications or games. Allow maximum of 30 characters.</source>
-      <translation type="unfinished">The name that will be displayed in emulated applications or games. Allow maximum of 30 characters.</translation>
+      <translation>Ta nazwa będzie wyświetlana w emulowanych aplikacjach lub grach. Pozwól na maksymalnie 30 znaków.</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="801"/>
       <source>Friendly phone name</source>
-      <translation type="unfinished">Friendly phone name</translation>
+      <translation>Przyjazna nazwa telefonu</translation>
     </message>
     <message>
       <location filename="../src/settings_dialog.ui" line="834"/>
