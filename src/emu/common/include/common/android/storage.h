f3ba51b893c3206afff79d1a97c071d45e43571c
pent0 <fewdspuckrito@gmail.com>
2021-11-29 00:01:14 +0700

    common/android: Fix tabs character mixed in storage.h

--- src/emu/common/include/common/android/storage.h
+++ src/emu/common/include/common/android/storage.h
@@ -20,54 +20,54 @@
 
 #include <common/android/jniutils.h>
 
-#include <vector>
-#include <string>
 #include <optional>
+#include <string>
+#include <vector>
 
 namespace eka2l1::common::android {
-	// To emphasize that Android storage mode strings are different, let's just use
-	// an enum.
-	enum class open_content_uri_mode {
-		READ = 0,  // "r"
-		READ_WRITE = 1,  // "rw"
-		READ_WRITE_TRUNCATE = 2  // "rwt"
-	};
+    // To emphasize that Android storage mode strings are different, let's just use
+    // an enum.
+    enum class open_content_uri_mode {
+        READ = 0, // "r"
+        READ_WRITE = 1, // "rw"
+        READ_WRITE_TRUNCATE = 2 // "rwt"
+    };
 
-	// Matches the constants in PpssppActivity.java.
-	enum class storage_error {
-		SUCCESS = 0,
-		UNKNOWN = -1,
-		NOT_FOUND = -2,
-		DISK_FULL = -3,
-		ALREADY_EXISTS = -4,
-	};
+    // Matches the constants in PpssppActivity.java.
+    enum class storage_error {
+        SUCCESS = 0,
+        UNKNOWN = -1,
+        NOT_FOUND = -2,
+        DISK_FULL = -3,
+        ALREADY_EXISTS = -4
+    };
 
-	inline storage_error storage_error_from_int(int ival) {
-		if (ival >= 0) {
-			return storage_error::SUCCESS;
-		} else {
-			return (storage_error)ival;
-		}
-	}
+    inline storage_error storage_error_from_int(int ival) {
+        if (ival >= 0) {
+            return storage_error::SUCCESS;
+        } else {
+            return (storage_error)ival;
+        }
+    }
 
-	int open_content_uri_fd(const std::string &uri, const open_content_uri_mode mode);
-	storage_error create_directory(const std::string &parentTreeUri, const std::string &dirName);
-	storage_error create_file(const std::string &parentTreeUri, const std::string &fileName);
-	storage_error move_file(const std::string &fileUri, const std::string &srcParentUri, const std::string &destParentUri);
-	storage_error copy_file(const std::string &fileUri, const std::string &destParentUri);
-	storage_error remove_file(const std::string &fileUri);
-	storage_error rename_file_to(const std::string &fileUri, const std::string &newName);
-	std::optional<std::string> get_file_info_as_string(const std::string &fileUri);
-	bool file_exists(const std::string &fileUri);
-	bool is_external_storage_preserved_legacy();
-	const char *error_to_string(storage_error error);
+    int open_content_uri_fd(const std::string &uri, const open_content_uri_mode mode);
+    storage_error create_directory(const std::string &parentTreeUri, const std::string &dirName);
+    storage_error create_file(const std::string &parentTreeUri, const std::string &fileName);
+    storage_error move_file(const std::string &fileUri, const std::string &srcParentUri, const std::string &destParentUri);
+    storage_error copy_file(const std::string &fileUri, const std::string &destParentUri);
+    storage_error remove_file(const std::string &fileUri);
+    storage_error rename_file_to(const std::string &fileUri, const std::string &newName);
+    std::optional<std::string> get_file_info_as_string(const std::string &fileUri);
+    bool file_exists(const std::string &fileUri);
+    bool is_external_storage_preserved_legacy();
+    const char *error_to_string(storage_error error);
 
-	std::vector<std::string> list_content_uri(const std::string &uri);
-	void register_storage_callbacks(JNIEnv * env);
+    std::vector<std::string> list_content_uri(const std::string &uri);
+    void register_storage_callbacks(JNIEnv *env);
 
-	void set_external_files_dir(const std::string &dir);
-	void set_external_dir(const std::string &dir);
+    void set_external_files_dir(const std::string &dir);
+    void set_external_dir(const std::string &dir);
 
-	std::string get_external_files_dir();
-	std::string get_external_dir();
+    std::string get_external_files_dir();
+    std::string get_external_dir();
 }
