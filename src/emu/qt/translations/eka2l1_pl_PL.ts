4d30e77084855cc5fbb2ff345c33ff9c1eaa9c8e
pent0 <25717050+pent0@users.noreply.github.com>
2022-02-04 19:22:37 +0700

    New translations eka2l1.ts (Polish)

--- src/emu/qt/translations/eka2l1_pl_PL.ts
+++ src/emu/qt/translations/eka2l1_pl_PL.ts
@@ -488,12 +488,12 @@
       <translation>Plik SIS (*.sis *.sisx)</translation>
     </message>
     <message>
-      <location filename="../src/mainwindow.cpp" line="1136"/>
+      <location filename="../src/mainwindow.cpp" line="1139"/>
       <source>Load theme failed!</source>
       <translation>Wczytywanie tematu nie powiodło się!</translation>
     </message>
     <message>
-      <location filename="../src/mainwindow.cpp" line="1136"/>
+      <location filename="../src/mainwindow.cpp" line="1139"/>
       <source>The Dark theme&apos;s style file can&apos;t be found!</source>
       <translation>Plik stylu ciemnego motywu nie został odnaleziony!</translation>
     </message>
