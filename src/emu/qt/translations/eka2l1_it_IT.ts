0d4c5f630b710d1936785e29ddbc4ecb3671b9e8
pent0 <25717050+pent0@users.noreply.github.com>
2022-02-04 19:22:36 +0700

    New translations eka2l1.ts (Italian)

--- src/emu/qt/translations/eka2l1_it_IT.ts
+++ src/emu/qt/translations/eka2l1_it_IT.ts
@@ -488,12 +488,12 @@
       <translation>File SIS (*.sis *.sisx)</translation>
     </message>
     <message>
-      <location filename="../src/mainwindow.cpp" line="1136"/>
+      <location filename="../src/mainwindow.cpp" line="1139"/>
       <source>Load theme failed!</source>
       <translation>Caricamento tema fallito!</translation>
     </message>
     <message>
-      <location filename="../src/mainwindow.cpp" line="1136"/>
+      <location filename="../src/mainwindow.cpp" line="1139"/>
       <source>The Dark theme&apos;s style file can&apos;t be found!</source>
       <translation>Impossibile trovare il file di stile del tema scuro!</translation>
     </message>
