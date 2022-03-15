28880f2ff5141cb5843c682816f7bf37b0d099ff
pent0 <25717050+pent0@users.noreply.github.com>
2022-03-16 02:30:06 +0700

    New translations eka2l1.ts (Ukrainian)

--- src/emu/qt/translations/eka2l1_uk_UA.ts
+++ src/emu/qt/translations/eka2l1_uk_UA.ts
@@ -19,7 +19,7 @@
     <message>
       <location filename="../src/symbian_input_dialog.ui" line="31"/>
       <source>Submit</source>
-      <translation>Відправити</translation>
+      <translation>Підтвердити</translation>
     </message>
   </context>
   <context>
