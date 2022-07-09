a2710e745a9b8b3e51be246daaa624eb0dec11cd
pent0 <l51492003231@163.com>
2022-07-10 02:27:34 +0700

    services: Add old backup stub server

---
+++ src/emu/services/include/services/backup/backup.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 EKA2L1 Team
+ * 
+ * This file is part of EKA2L1 project.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <kernel/server.h>
+#include <services/framework.h>
+
+namespace eka2l1 {
+    class backup_old_server : public service::typical_server {
+    public:
+        explicit backup_old_server(eka2l1::system *sys);
+        void connect(service::ipc_context &context) override;
+    };
+
+    struct backup_old_session : public service::typical_session {
+        explicit backup_old_session(service::typical_server *serv, const kernel::uid ss_id, epoc::version client_version);
+
+        void fetch(service::ipc_context *ctx) override;
+    };
+}
