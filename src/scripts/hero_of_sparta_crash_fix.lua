50be5bbddd5b7f275e146369cdbff04a3de453f9
pent0 <l51492003231@163.com>
2022-08-13 03:27:06 +0700

    script: Add Hero of Sparta crash fix

---
+++ src/scripts/hero_of_sparta_crash_fix.lua
@@ -0,0 +1,14 @@
+local common = require('eka2l1.common')
+local evt = require('eka2l1.events')
+local cpu = require('eka2l1.cpu')
+
+local ffi = require('ffi')
+
+function fixTemporaryFileBufferTooSmall()
+    -- The file buffer used to store the data is full too fast with the size of 0x80000 bytes (~0.5mb)
+    -- On a real phone, maybe it overflows, but the OS is less forgiving, or it does not at all. But increase should do it right.
+	cpu.setReg(0, 6 * 1024 * 1024)      -- 6MB
+end
+
+common.log('Script enabled: Hero of Sparta (Gameloft) crash fix.')
+evt.registerBreakpointHook('heroofsparta.exe', 0x000886B3, 0, 0, fixTemporaryFileBufferTooSmall, 0x612393CE)
