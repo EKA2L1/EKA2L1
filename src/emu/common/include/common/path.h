61f8f6ebd83335f7a36b9403749825b4db32b701
Romain TISSERAND <romain.tisserand@gmail.com>
2023-09-11 16:48:08 +0200

    GCC 13 fixes, including RectangleBinPack lunasvg submodules bumps (#505)
    * GCC 13 fixes, including RectangleBinPack and lunasvg submodules required bumps
    
    * Update lunasvg
    
    ---------
    
    Co-authored-by: Nikita Shakarun <nikita36078@gmail.com>

--- src/emu/common/include/common/path.h
+++ src/emu/common/include/common/path.h
@@ -20,6 +20,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <functional>
 #include <stdexcept>
 #include <string>
--- src/emu/j2me/include/j2me/applist.h
+++ src/emu/j2me/include/j2me/applist.h
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
 #include <optional>
